a52fd5ea0fad4e60a3deb79d2af4a2179d78e7ba
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-01-19 16:57:45 +0100

    feat: adde the initial EnemyEnum

---
+++ src/main/java/it/unibo/crabinv/Model/Enemies/EnemyType.java
@@ -0,0 +1,6 @@
+package it.unibo.crabinv.Model.Enemies;
+
+public enum EnemyType {
+    SERVANT,
+    ELITECRAB,
+}
---
+++ src/main/java/it/unibo/crabinv/Model/Enemy.java
@@ -0,0 +1,6 @@
+package it.unibo.crabinv.Model;
+import it.unibo.crabinv.Model.Enemies.EnemyType;
+
+public interface Enemy {
+    public EnemyType getEnemyType();
+}
