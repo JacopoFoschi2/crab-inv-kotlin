8138d24c83f775094c03f7910bd4e911660f7ee7
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-19 19:23:38 +0100

    feat: implemented the Localization class to handle localization across the app

--- src/main/java/it/unibo/crabinv/i18n/Localization.java
+++ src/main/java/it/unibo/crabinv/i18n/Localization.java
@@ -1,4 +1,43 @@
 package it.unibo.crabinv.i18n;
+import java.util.Locale;
+import java.util.Locale.*;
+import java.util.ResourceBundle;
 
 public class Localization {
+    private ResourceBundle messages;
+    private Locale currentLocale;
+    /**
+     * Nested class that lists all the currently supported locales
+     * to ensure use of only implemented and correctly formed ones
+     */
+    public static final class SUPPORTED_LOCALES {
+            public static final Locale ENGLISH = new Builder().setLanguage("en").setRegion("US").build();
+            public static final Locale ITALIAN = new Builder().setLanguage("it").setRegion("IT").build();
+    };
+
+    /**
+     * Creates new Localization instance based on input locale
+     * @param locale is to be chosen from SUPPORTED_LOCALES
+     */
+    public Localization(Locale locale) {
+        setLocale(locale);
+    }
+
+    /**
+     * Changes currently loaded locale
+     * @param locale is to be chosen from SUPPORTED_LOCALES
+     */
+    public void setLocale(Locale locale) {
+        currentLocale = locale;
+        messages = ResourceBundle.getBundle("i18n.messages", currentLocale);
+    }
+
+    /**
+     * Gets string based on the current locale
+     * @param key the key of the wanted string
+     * @return the wanted string
+     */
+    public String get(String key) {
+        return messages.getString(key);
+    }
 }
