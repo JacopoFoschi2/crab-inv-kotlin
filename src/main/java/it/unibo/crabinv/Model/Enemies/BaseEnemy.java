03d645c2f9df3d9761ffaffc6d35f007a6330212
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-01-20 15:51:01 +0100

    feat: adde the class of the point in model.utils

--- src/main/java/it/unibo/crabinv/Model/Enemies/BaseEnemy.java
+++ src/main/java/it/unibo/crabinv/Model/Enemies/BaseEnemy.java
@@ -2,8 +2,12 @@ package it.unibo.crabinv.Model.Enemies;
 
 import it.unibo.crabinv.Model.Enemy;
 
-public class CrabShooter implements Enemy {
-    
+public class BaseEnemy implements Enemy {
+    private int health = 1;
+    private int coordinateX;
+    private int coordinateY;
+
+
     @Override
     public EnemyType getEnemyType() {
         return EnemyType.SERVANT;
---
+++ src/main/java/it/unibo/crabinv/Model/Entity.java
@@ -0,0 +1,9 @@
+package it.unibo.crabinv.Model;
+
+
+public interface Entity {
+    public int getHealthPoints();
+    public boolean isAlive();
+    public  getCoordinates();
+
+}
