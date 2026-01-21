42e7d60bc57ac6b1ee12891d36f6df091b8f265d
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-21 21:25:07 +0100

    fix: fixed the two play methods to work with new enums

--- src/main/java/it/unibo/crabinv/Model/audio/SoundService.java
+++ src/main/java/it/unibo/crabinv/Model/audio/SoundService.java
@@ -4,13 +4,13 @@ package it.unibo.crabinv.Model.audio;
  * Interface that handles all basic sound related operations
  */
 public interface SoundService {
-    void playBGM(String musicName);
+    void playBGM(BGMTracks musicName);
     void stopBGM();
     void setBGMVolume(double volume);
     double getBGMVolume();
     void toggleMuteBGM();
     boolean isBGMMuted();
-    void playSfx(String effectName);
+    void playSfx(SFXTracks effectName);
     void setSFXVolume(double volume);
     double getSFXVolume();
     void toggleMuteSFX();
