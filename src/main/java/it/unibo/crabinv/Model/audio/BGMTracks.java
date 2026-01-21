fa4280ff0315f1f19bd901bd67a3223ef24e2264
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-21 21:35:58 +0100

    feat: implemented BGMTracks enum

--- src/main/java/it/unibo/crabinv/Model/audio/BGMTracks.java
+++ src/main/java/it/unibo/crabinv/Model/audio/BGMTracks.java
@@ -1,4 +1,15 @@
 package it.unibo.crabinv.Model.audio;
 
 public enum BGMTracks {
+    LEVEL("/audio/BGM/main_bgm.mp3");
+
+    private final String path;
+
+    BGMTracks(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
 }
