45cf07a950e2b1e85b693ecdd25b2eff1df5e034
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-21 17:48:46 +0100

    feat: implemented SoundService interface

--- src/main/java/it/unibo/crabinv/Model/audio/SoundService.java
+++ src/main/java/it/unibo/crabinv/Model/audio/SoundService.java
@@ -1,5 +1,15 @@
 package it.unibo.crabinv.Model.audio;
 
 public interface SoundService {
-
+    void playBGM();
+    void stopBGM();
+    void setBGMVolume(double volume);
+    double getBGMVolume();
+    void toggleMuteBGM();
+    boolean isBGMMuted();
+    void playSfx(String effectName);
+    void setSFXVolume(double volume);
+    double getSFXVolume();
+    void toggleMuteSFX();
+    boolean isSFXMuted();
 }
