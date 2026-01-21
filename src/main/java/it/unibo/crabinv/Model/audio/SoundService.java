0add90441d4f20b11748bce09d76aa5075823fac
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-21 20:39:40 +0100

    fix: turned volume getters from double to int

--- src/main/java/it/unibo/crabinv/Model/audio/SoundService.java
+++ src/main/java/it/unibo/crabinv/Model/audio/SoundService.java
@@ -7,12 +7,12 @@ public interface SoundService {
     void playBGM(String musicName);
     void stopBGM();
     void setBGMVolume(int volume);
-    double getBGMVolume();
+    int getBGMVolume();
     void toggleMuteBGM();
     boolean isBGMMuted();
     void playSfx(String effectName);
     void setSFXVolume(int volume);
-    double getSFXVolume();
+    int getSFXVolume();
     void toggleMuteSFX();
     boolean isSFXMuted();
 }
