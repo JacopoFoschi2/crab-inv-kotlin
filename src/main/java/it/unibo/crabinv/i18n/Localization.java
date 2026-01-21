71cd5dccf649dde0f3b0da4da9a266b3f1adac02
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-21 15:52:25 +0100

    refactor: turned SUPPORTED_LOCALES into type safe enum to ensure correct usage and renamed get into getString

--- src/main/java/it/unibo/crabinv/i18n/Localization.java
+++ src/main/java/it/unibo/crabinv/i18n/Localization.java
@@ -1,33 +1,40 @@
 package it.unibo.crabinv.i18n;
 
 import java.util.Locale;
-import java.util.Locale.*;
+import java.util.Locale.Builder;
 import java.util.ResourceBundle;
 
 public class Localization {
     private static Localization localization;
     private ResourceBundle messages;
-    private Locale currentLocale;
-    /**
-     * Nested class that lists all the currently supported locales
-     * to ensure use of only implemented and correctly formed ones
-     */
-    public static final class SUPPORTED_LOCALES {
-            public static final Locale ENGLISH = new Builder().setLanguage("en").setRegion("US").build();
-            public static final Locale ITALIAN = new Builder().setLanguage("it").setRegion("IT").build();
+    private SUPPORTED_LOCALES currentLocale;
+
+    public enum SUPPORTED_LOCALES {
+        ENGLISH(new Builder().setLanguage("en").setRegion("US").build()),
+        ITALIAN(new Builder().setLanguage("it").setRegion("IT").build());
+
+        private final Locale locale;
+
+        SUPPORTED_LOCALES(Locale locale) {
+            this.locale = locale;
+        }
+
+        public Locale getLocale() {
+            return locale;
+        }
     }
 
     /**
      * Creates new Localization instance based on input locale
      * @param locale is to be chosen from SUPPORTED_LOCALES
      */
-    private Localization(Locale locale) {
+    private Localization(SUPPORTED_LOCALES locale) {
         setLocale(locale);
     }
 
     public static Localization getInstance() {
         if (localization == null) {
-            localization = new Localization(Locale.ENGLISH);
+            localization = new Localization(SUPPORTED_LOCALES.ENGLISH);
         }
         return localization;
     }
@@ -36,9 +43,9 @@ public class Localization {
      * Changes currently loaded locale
      * @param locale is to be chosen from SUPPORTED_LOCALES
      */
-    public void setLocale(Locale locale) {
+    public void setLocale(SUPPORTED_LOCALES locale) {
         currentLocale = locale;
-        messages = ResourceBundle.getBundle("i18n.messages", currentLocale);
+        messages = ResourceBundle.getBundle("i18n.messages", currentLocale.getLocale());
     }
 
     /**
@@ -46,7 +53,7 @@ public class Localization {
      * @param key the key of the wanted string. Take it from TextKeys
      * @return the wanted string
      */
-    public String get(String key) {
+    public String getString(String key) {
         return messages.getString(key);
     }
 }
