59f745aaf573bb5aaac97098498886fe12424647
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-22 12:18:24 +0100

    feat: implemented SFXTracks

--- src/main/java/it/unibo/crabinv/Model/audio/SFXTracks.java
+++ src/main/java/it/unibo/crabinv/Model/audio/SFXTracks.java
@@ -1,4 +1,19 @@
 package it.unibo.crabinv.Model.audio;
 
 public enum SFXTracks {
+    MENU_OPEN("/audio/SFX/menu_open.mp3"),
+    MENU_CLOSE("/audio/SFX/menu_close.mp3"),
+    MENU_SELECT("/audio/SFX/menu_select.mp3"),
+    SHOT_ENEMY("/audio/SFX/shot_enemy.mp3"),
+    SHOT_PLAYER("/audio/SFX/shot_player.mp3");
+
+    private final String path;
+
+    SFXTracks(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
 }
