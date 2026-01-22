3917c7e8fb115d01e783d0db598ae3f460682ffb
Jonathan Crescentini <jonathan.crescentini@studio.unibo.it>
2026-01-22 18:50:54 +0100

    feat: completed interface GameSession

--- src/main/java/it/unibo/crabinv/Model/GameSession.java
+++ src/main/java/it/unibo/crabinv/Model/GameSession.java
@@ -1,10 +1,42 @@
 package it.unibo.crabinv.Model;
 
 public interface GameSession {
-    public int getCurrentLevel();
-    public boolean isGameOver();
-    public int getNextLevel();
-    public int getCurrency(); //valuta della run
-    public int getPlayerHealth();
+    /*Level managing*/
+    int getCurrentLevel();
+    boolean isGameOver();
+    int getNextLevel();
+
+    /*Active run player stats*/
+    int getCurrency();
+    int getPlayerHealth();
+
+    /**
+     * Advances CurrentLevel and NextLevel
+     */
+    void advanceLevel();
+
+    /**
+     * Sets GameOver to true
+     */
+    void setGameOver();
+
+    /**
+     * Adds an amount to current Currency
+     * @param amount the currency to add
+     */
+    void addCurrency(int amount);
+
+    /**
+     * Adds an amount to current PlayerHealth
+     * @param amount the amount to add
+     */
+    void addPlayerHealth(int amount);
+
+    /**
+     * Subtracts an amount from current PlayerHealth
+     * @param amount the amount to subtract
+     */
+    void subPlayerHealth(int amount);
+
 }
 
--- src/main/java/it/unibo/crabinv/Model/UserProfile.java
+++ src/main/java/it/unibo/crabinv/Model/UserProfile.java
@@ -7,11 +7,11 @@ public interface UserProfile {
      * Sums the amount to the stored currency
      * @param amount the amount of currency to add
      * */
-    void addCurrency( int amount);
+    void addCurrency (int amount);
     /**
      * Sets the selected powerUp to locked or unlocked
      * @param powerUp Id of the power up
      * @param unlocked status to apply to the powerUp
      * */
-    void managePowerUp ( String powerUp, boolean unlocked);
+    void managePowerUp (String powerUp, boolean unlocked);
 }
