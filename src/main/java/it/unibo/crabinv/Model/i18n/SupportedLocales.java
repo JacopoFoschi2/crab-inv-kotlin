04bdddcf127f152656f34de1d57dd16e540b3674
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-23 19:43:22 +0100

    feat: added localized names for locale

--- src/main/java/it/unibo/crabinv/Model/i18n/SupportedLocales.java
+++ src/main/java/it/unibo/crabinv/Model/i18n/SupportedLocales.java
@@ -6,15 +6,17 @@ import java.util.Locale;
  * Lists all currently supported locales to ensure correct usage
  */
 public enum SupportedLocales {
-    ENGLISH(new Locale.Builder().setLanguage("en").setRegion("US").build(), "/flags/flag_en.png"),
-    ITALIAN(new Locale.Builder().setLanguage("it").setRegion("IT").build(), "/flags/flag_it.png"),;
+    ENGLISH(new Locale.Builder().setLanguage("en").setRegion("US").build(), "/flags/flag_en.png", "ENGLISH"),
+    ITALIAN(new Locale.Builder().setLanguage("it").setRegion("IT").build(), "/flags/flag_it.png", "ITALIANO"),;
 
     private final Locale locale;
     private final String imagePath;
+    private final String localizedName;
 
-    SupportedLocales(Locale locale, String imagePath) {
+    SupportedLocales(Locale locale, String imagePath, String localizedName) {
         this.locale = locale;
         this.imagePath = imagePath;
+        this.localizedName = localizedName;
     }
 
     public Locale getLocale() {
@@ -23,5 +25,9 @@ public enum SupportedLocales {
 
     public String getImagePath() {
         return imagePath;
+    }
+
+    public String getLocalizedName() {
+        return localizedName;
     }
 }
