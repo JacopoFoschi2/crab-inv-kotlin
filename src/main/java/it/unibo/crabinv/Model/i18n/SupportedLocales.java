d2640ad847eb14573992fbc25f414d14efe3eee3
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-23 19:25:44 +0100

    feat: added imagePath to SupportedLocales

--- src/main/java/it/unibo/crabinv/Model/i18n/SupportedLocales.java
+++ src/main/java/it/unibo/crabinv/Model/i18n/SupportedLocales.java
@@ -6,16 +6,22 @@ import java.util.Locale;
  * Lists all currently supported locales to ensure correct usage
  */
 public enum SupportedLocales {
-    ENGLISH(new Locale.Builder().setLanguage("en").setRegion("US").build()),
-    ITALIAN(new Locale.Builder().setLanguage("it").setRegion("IT").build());
+    ENGLISH(new Locale.Builder().setLanguage("en").setRegion("US").build(), "/flags/flag_en.png"),
+    ITALIAN(new Locale.Builder().setLanguage("it").setRegion("IT").build(), "/flags/flag_it.png"),;
 
     private final Locale locale;
+    private final String imagePath;
 
-    SupportedLocales(Locale locale) {
+    SupportedLocales(Locale locale, String imagePath) {
         this.locale = locale;
+        this.imagePath = imagePath;
     }
 
     public Locale getLocale() {
         return locale;
+    }
+
+    public String getImagePath() {
+        return imagePath;
     }
 }
