665ebee94ac709f86d44f18c180c587f5284162c
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-23 11:13:49 +0100

    feat: added base skeletal setup in App

--- src/main/java/it/unibo/crabinv/App.java
+++ src/main/java/it/unibo/crabinv/App.java
@@ -1,14 +1,19 @@
 package it.unibo.crabinv;
 
+import it.unibo.crabinv.Controller.audio.AudioController;
+import it.unibo.crabinv.Model.audio.JavaFXSoundManager;
+import it.unibo.crabinv.Model.i18n.Localization;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
 public class App extends Application {
     @Override
     public void start(Stage stage) throws Exception {
-
+        Localization loc = new Localization();
+        AudioController audio = new AudioController(new JavaFXSoundManager());
     }
 
     static void main() {
+        launch();
     }
 }
