adf8bf834763b8c1b4d1bafbcdeefe11980a44e6
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-25 14:27:49 +0100

    feat: implemented SceneManager

--- src/main/java/it/unibo/crabinv/SceneManager.java
+++ src/main/java/it/unibo/crabinv/SceneManager.java
@@ -1,5 +1,23 @@
 package it.unibo.crabinv;
 
-public class SceneManager {
+import it.unibo.crabinv.Controller.i18n.LocalizationController;
+import it.unibo.crabinv.View.LanguageSelection;
+import javafx.scene.layout.StackPane;
 
+public class SceneManager {
+    private final StackPane root;
+    private final LocalizationController loc;
+
+    public SceneManager(StackPane root, LocalizationController loc) {
+        this.root = root;
+        this.loc = loc;
+    }
+
+    public void showLanguageSelection() {
+        root.getChildren().setAll(new LanguageSelection(loc,this).getView());
+    }
+
+    public void showMainMenu() {
+        root.getChildren().setAll(new StackPane());
+    }
 }
