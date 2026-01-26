e4bdf6f72b319b7a88b8a8e973e4820cdf8a60d5
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-26 11:40:14 +0100

    fix: rewrote move method to work with a delta and bounds

--- src/main/java/it/unibo/crabinv/Model/entity/Movable.java
+++ src/main/java/it/unibo/crabinv/Model/entity/Movable.java
@@ -2,7 +2,10 @@ package it.unibo.crabinv.Model.entity;
 
 public interface Movable {
     /**
-     * Handles movement for every tick
+     * Handles movement for every tick based on given delta
+     * @param dt the delta of movement, which is either +1, 0 or -1
+     * @param minBound the minimal bound that the entity cannot surpass
+     * @param maxBound the maximal bound that the entity cannot surpass
      */
-    void move();
+    void move(double dt, double minBound, double maxBound);
 }
