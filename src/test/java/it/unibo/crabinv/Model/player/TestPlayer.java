5603425bd7c526a608483ad89ab420a187c8f133
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-26 14:13:59 +0100

    feat: implemented TestPlayer with various tests

--- src/test/java/it/unibo/crabinv/Model/player/TestPlayer.java
+++ src/test/java/it/unibo/crabinv/Model/player/TestPlayer.java
@@ -1,4 +1,60 @@
 package it.unibo.crabinv.Model.player;
 
+import it.unibo.crabinv.Model.entity.Delta;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class TestPlayer {
+    Player player;
+
+    @BeforeEach
+    public void setup() {
+        player = new Player(1,0,0,1,1);
+    }
+
+    @Test
+    public void TestPlayerMovement() {
+        player.move(Delta.INCREASE, -1, 1);
+        Assertions.assertEquals(1, player.getX());
+    }
+
+    @Test
+    public void TestOutOfBounds() {
+        player.move(Delta.INCREASE, -1, 1);
+        Assertions.assertEquals(1, player.getX());
+        player.move(Delta.DECREASE, -1, 1);
+        Assertions.assertEquals(0, player.getX());
+        player.move(Delta.DECREASE, -1, 1);
+        Assertions.assertEquals(-1, player.getX());
+        player.move(Delta.DECREASE, -1, 1);
+        Assertions.assertEquals(-1, player.getX());
+    }
+
+    @Test
+    public void TestNoMovement() {
+        player.move(Delta.NO_ACTION, -1, 1);
+        Assertions.assertEquals(0, player.getX());
+    }
+
+    @Test
+    public void TestShoot() {
+        Assertions.assertTrue(player.isAbleToShoot());
+        player.shoot();
+        Assertions.assertFalse(player.isAbleToShoot());
+    }
+
+    @Test
+    public void TestDeath() {
+        player.onCollisionWith(player);
+        Assertions.assertFalse(player.isAlive());
+    }
+
+    @Test
+    public void TestReload() {
+        player.shoot();
+        Assertions.assertFalse(player.isAbleToShoot());
+        player.tick();
+        Assertions.assertTrue(player.isAbleToShoot());
+    }
 }
