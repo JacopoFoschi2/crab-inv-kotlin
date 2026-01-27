8fb1cbfeb01c77c64b53a1bd9c5ab9276b89fcf2
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-27 11:57:33 +0100

    feat: turned AppSettings into record class and implemented it

--- src/main/java/it/unibo/crabinv/config/AppSettings.java
+++ src/main/java/it/unibo/crabinv/config/AppSettings.java
@@ -1,5 +1,16 @@
 package it.unibo.crabinv.config;
 
-public class AppSettings {
+import it.unibo.crabinv.Model.i18n.SupportedLocales;
 
-}
+/**
+ * Helper class that copies the current state of volume and localization to
+ * be then read by {@link SettingsFileManager} to save the state
+ * before app closing.
+ * It has the explicit purpose of streamlining the saving procedure by making it easier.
+ */
+public record AppSettings(
+        int bgmVolume,
+        int sfxVolume,
+        boolean isBGMMuted,
+        boolean isSFXMuted,
+        SupportedLocales locales) { }
