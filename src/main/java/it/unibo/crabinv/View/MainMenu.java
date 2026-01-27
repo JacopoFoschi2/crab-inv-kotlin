744bf548bbb58388dd40dd115ca4c87013675d00
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-01-27 19:21:14 +0100

    feat: added the skeleton of the menu

--- src/main/java/it/unibo/crabinv/View/MainMenu.java
+++ src/main/java/it/unibo/crabinv/View/MainMenu.java
@@ -2,18 +2,27 @@ package it.unibo.crabinv.View;
 
 import it.unibo.crabinv.Controller.audio.AudioController;
 import it.unibo.crabinv.Controller.i18n.LocalizationController;
+import it.unibo.crabinv.Model.audio.SFXTracks;
 import it.unibo.crabinv.Model.i18n.TextKeys;
 import it.unibo.crabinv.SceneManager;
+import javafx.geometry.Pos;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import java.util.Objects;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
 
 public class MainMenu {
     /*
     needs to contain:
     -play
-    -shop? (actually here?)
+    -shop
     -Run Log
-    -settings
+    -settings  -> volume and language settings
     -exit game
      */
     private final SceneManager sceneManager;
@@ -28,7 +37,55 @@ public class MainMenu {
 
     public Pane getView() {
         Pane pane = new StackPane();
-        loc.getString(TextKeys.PLAY);
+        VBox mainColumn = new VBox(25);
+        mainColumn.setAlignment(Pos.CENTER);
 
+
+        mainColumn.getChildren().add(
+                createMenuButton(TextKeys.PLAY, () -> System.out.println("PLAY"))
+        );
+        /*
+        pane.getChildren().addAll(
+                ,
+                createMenuButton(TextKeys.SHOP),
+                createMenuButton(TextKeys.RUN_LOG),
+                createMenuButton(TextKeys.SETTINGS),
+                createMenuButton(TextKeys.EXIT_GAME)
+        );
+         */
+        pane.getChildren().add(mainColumn);
+
+        return pane;
     }
+
+    private Button createMenuButton(TextKeys key, Runnable action) {
+        Label label = new Label(loc.getString(key));
+        label.getStyleClass().add("menu-button-label");
+
+
+        Image borderImage = new Image(Objects.requireNonNull(
+                getClass().getResourceAsStream("/uiImages/frameMenuButton.png")
+        ));
+
+        ImageView background = new ImageView(borderImage);
+        background.setPreserveRatio(true);
+        background.setFitWidth(300);
+
+        StackPane composition = new StackPane(background,label); //composition of the image with the text
+
+        Button menuButton = new Button();
+        menuButton.setGraphic(composition);
+        menuButton.getStyleClass().add("menu-button");
+        menuButton.focusedProperty().addListener((_, _, newValue) -> {
+            if (newValue) {
+                audio.playSFX(SFXTracks.MENU_HOVER);
+            }
+        });
+
+        menuButton.setOnAction(e -> {
+            audio.playSFX(SFXTracks.MENU_SELECT);
+        });
+        return menuButton;
+    }
+
 }
