6563f575ff8a60006f08d6474a58c2368eae57d6
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-28 18:24:55 +0100

    feat: created class Shield

---
+++ src/main/java/it/unibo/crabinv/Model/shield/Shield.java
@@ -0,0 +1,13 @@
+package it.unibo.crabinv.Model.shield;
+
+import it.unibo.crabinv.Model.entity.AbstractEntity;
+
+public class Shield extends AbstractEntity {
+
+    /**
+     * Creates the shield
+     */
+    public Shield(int maxHealth, double x, double y) {
+        super(maxHealth, x, y);
+    }
+}
