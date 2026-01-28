181d457ed4513214c899677a5f5799e306791883
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-28 10:18:18 +0100

    feat: partly implemented Settings

--- src/main/java/it/unibo/crabinv/View/Settings.java
+++ src/main/java/it/unibo/crabinv/View/Settings.java
@@ -3,6 +3,10 @@ package it.unibo.crabinv.View;
 import it.unibo.crabinv.Controller.audio.AudioController;
 import it.unibo.crabinv.Controller.i18n.LocalizationController;
 import it.unibo.crabinv.SceneManager;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
@@ -21,5 +25,19 @@ public class Settings {
     public Pane getView() {
         Pane pane = new StackPane();
         VBox mainColumn = new VBox(20);
+        Label title = new Label("SETTINGS");
+        HBox bgmVolume = new HBox(20);
+        Slider bgmVolumeSlider = new Slider(0, 100, audio.getBGMVolume());
+        Label bgmTitle = new Label("BGM Volume");
+        bgmVolume.getChildren().addAll(bgmVolumeSlider, bgmTitle);
+        HBox sfxVolume = new HBox(20);
+        Slider sfxVolumeSlider = new Slider(0, 100, audio.getSFXVolume());
+        Label sfxTitle = new Label("SFX Volume");
+        sfxVolume.getChildren().addAll(sfxVolumeSlider, sfxTitle);
+        CheckBox bgmMute = new CheckBox("Mute BGM");
+        CheckBox sfxMute = new CheckBox("Mute SFX");
+        mainColumn.getChildren().addAll(title, bgmVolume, sfxVolume, bgmMute, sfxMute);
+        pane.getChildren().addAll(mainColumn);
+        return pane;
     }
 }
