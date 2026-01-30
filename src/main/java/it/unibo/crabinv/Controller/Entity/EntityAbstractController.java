f551163456e9fbb583fea68b5fe5222c138c220a
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-30 15:28:34 +0100

    feat: created EntityAbstractController

---
+++ src/main/java/it/unibo/crabinv/Controller/Entity/EntityAbstractController.java
@@ -0,0 +1,18 @@
+package it.unibo.crabinv.Controller.Entity;
+
+import it.unibo.crabinv.Model.entity.Delta;
+
+/**
+ * Provides the methods an EntityController should absolutely implement in one of its overloads
+ */
+public class EntityAbstractController implements EntityController {
+    @Override
+    public void update(boolean firePressed, Delta delta) {
+
+    }
+
+    @Override
+    public void update(Delta delta) {
+
+    }
+}
