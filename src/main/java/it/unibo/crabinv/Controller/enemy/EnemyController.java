86f17d25b414236af7e30264dafde43fdfe4f44d
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-01-30 15:56:54 +0100

    chore: removed the controller of the enemy

--- src/main/java/it/unibo/crabinv/Controller/enemy/EnemyController.java
+++
@@ -1,9 +0,0 @@
-package it.unibo.crabinv.Controller.enemy;
-
-public class EnemyController {
-
-    /*
-    TODO: here we need the Calculator of the shoot for the enemy(33% of possibility of shoot),
-      the movement of the enemy needs to go here
-     */
-}
