e7835914530096a6ebe81c5d937e0da2f2bd2984
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-01-30 15:28:51 +0100

    feat: implemented EntityController

--- src/main/java/it/unibo/crabinv/Controller/Entity/EntityController.java
+++ src/main/java/it/unibo/crabinv/Controller/Entity/EntityController.java
@@ -1,5 +1,21 @@
 package it.unibo.crabinv.Controller.Entity;
 
-public interface EntityController {
+import it.unibo.crabinv.Model.entity.Delta;
 
+/**
+ * Provides methods to control an entity standardizing the way they should be controlled
+ */
+public interface EntityController {
+    /**
+     * Updates the status of something that receives inputs, such as the player
+     * @param firePressed
+     * @param delta
+     */
+    void update(boolean firePressed, Delta delta);
+
+    /**
+     * Updates the status of something that doesn't receive inputs,
+     * and therefore has constant movement
+     */
+    void update(Delta delta);
 }
