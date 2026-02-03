2b914ef15d2bd24008cf744fe6275007c5e5ae2b
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-02-03 16:00:28 +0100

    feat: added all the methods of entity model to entityController

--- src/main/java/it/unibo/crabinv/Controller/entity/EntityAbstractController.java
+++ src/main/java/it/unibo/crabinv/Controller/entity/EntityAbstractController.java
@@ -1,6 +1,7 @@
 package it.unibo.crabinv.Controller.entity;
 
 import it.unibo.crabinv.Model.entity.Delta;
+import it.unibo.crabinv.Model.entity.Entity;
 
 /**
  * Provides the methods an EntityController should absolutely implement in one of its overloads
@@ -14,5 +15,35 @@ public class EntityAbstractController implements EntityController {
     @Override
     public void update(Delta delta) {
 
+    }
+
+    @Override
+    public boolean isAlive() {
+        return false;
+    }
+
+    @Override
+    public void onCollisionWith(Entity other) {
+
+    }
+
+    @Override
+    public int getHealth() {
+        return 0;
+    }
+
+    @Override
+    public int getMaxHealth() {
+        return 0;
+    }
+
+    @Override
+    public double getX() {
+        return 0;
+    }
+
+    @Override
+    public double getY() {
+        return 0;
     }
 }
--- src/main/java/it/unibo/crabinv/Controller/entity/EntityController.java
+++ src/main/java/it/unibo/crabinv/Controller/entity/EntityController.java
@@ -1,6 +1,7 @@
 package it.unibo.crabinv.Controller.entity;
 
 import it.unibo.crabinv.Model.entity.Delta;
+import it.unibo.crabinv.Model.entity.Entity;
 
 /**
  * Provides methods to control an entity standardizing the way they should be controlled
@@ -18,4 +19,35 @@ public interface EntityController {
      * and therefore has constant movement
      */
     void update(Delta delta);
+
+    /**
+     * @return if the entity is alive
+     */
+    public boolean isAlive();
+
+    /**
+     * Calls on collisionWith method of the entity.
+     * @param other the other entity it collided with
+     */
+    public void onCollisionWith(Entity other);
+
+    /**
+     * @return the current health of the entity
+     */
+    public int getHealth();
+
+    /**
+     * @return the max ealth of the entity
+     */
+    public int getMaxHealth();
+
+    /**
+     * @return the x coordinate of the entity
+     */
+    public double getX();
+
+    /**
+     * @return the y coordinate of the enity
+     */
+    public double getY();
 }
