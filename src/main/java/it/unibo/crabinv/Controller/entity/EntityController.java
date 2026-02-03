f9de50e43d16c473fc3439bcc4e1d94b9eec75e6
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-02-03 18:48:49 +0100

    docs: fixed javadoc of EntityController

--- src/main/java/it/unibo/crabinv/Controller/entity/EntityController.java
+++ src/main/java/it/unibo/crabinv/Controller/entity/EntityController.java
@@ -1,24 +1,11 @@
 package it.unibo.crabinv.Controller.entity;
 
-import it.unibo.crabinv.Model.entity.Delta;
 import it.unibo.crabinv.Model.entity.Entity;
 
 /**
  * Provides methods to control an entity standardizing the way they should be controlled
  */
 public interface EntityController {
-    /**
-     * Updates the status of something that receives inputs, such as the player
-     * @param firePressed
-     * @param delta
-     */
-    void update(boolean firePressed, Delta delta);
-
-    /**
-     * Updates the status of something that doesn't receive inputs,
-     * and therefore has constant movement
-     */
-    void update(Delta delta);
 
     /**
      * @return if the entity is alive
@@ -37,7 +24,7 @@ public interface EntityController {
     public int getHealth();
 
     /**
-     * @return the max ealth of the entity
+     * @return the max health of the entity
      */
     public int getMaxHealth();
 
