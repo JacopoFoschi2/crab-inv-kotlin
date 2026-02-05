6951aaf2c0aec7eeb42fe49e8e0d4f1ce5560a3c
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-02-05 17:45:27 +0100

    refactor: removed redoundant public in Entity interface

--- src/main/java/it/unibo/crabinv/Model/entity/Entity.java
+++ src/main/java/it/unibo/crabinv/Model/entity/Entity.java
@@ -8,32 +8,32 @@ public interface Entity {
     /**
      * @return the current amount of HP
      */
-    public int getHealth();
+    int getHealth();
 
     /**
      * @return the max amount of HP an entity has
      */
-    public int getMaxHealth();
+    int getMaxHealth();
 
     /**
      * Checks if the entity is still alive
      * @return true if alive, false if not
      */
-    public boolean isAlive();
+    boolean isAlive();
 
     /**
      * @return the x coordinate of the entity
      */
-    public double getX();
+    double getX();
 
     /**
      * @return the y coordinate of the entity
      */
-    public double getY();
+    double getY();
 
     /**
      * Make the entity suffer the amount of damage in input
      * @param damage the damage it should receive
      */
-    public void takeDamage(int damage);
+    void takeDamage(int damage);
 }
