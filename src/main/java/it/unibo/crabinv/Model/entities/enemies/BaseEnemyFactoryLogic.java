e0f14cdda2524c2502970196338fc09768a58e2b
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-02-06 19:48:03 +0100

    fix: fixed the enemy factory

--- src/main/java/it/unibo/crabinv/Model/entities/enemies/BaseEnemyFactoryLogic.java
+++ src/main/java/it/unibo/crabinv/Model/entities/enemies/BaseEnemyFactoryLogic.java
@@ -3,20 +3,10 @@ package it.unibo.crabinv.Model.entities.enemies;
 import it.unibo.crabinv.Model.entities.entity.EntitySprites;
 
 public class BaseEnemyFactoryLogic implements EnemyFactory{
+
     @Override
-    public Enemy createEnemy(EnemyType type, double x, double y) {
-        return switch (type){
-            case SERVANT ->  EnemyImpl.builder()
-                    .x(x)
-                    .y(y)
-                    .maxHealth(1)
-                    .health(1)
-                    .speed(0.5)
-                    .fireRate(1)
-                    .type(type)
-                    .radius(10)
-                    .sprite(EntitySprites.ENEMY_SERVANT)
-                    .build();
-        };
+    public Enemy createEnemy(EnemyType type, double x, double y, double minBound, double maxBound) {
+        return new EnemyImpl(x,y, 1, 1.0, type, 1, 0.01,
+                minBound, maxBound, EntitySprites.ENEMY_SERVANT);
     }
 }
