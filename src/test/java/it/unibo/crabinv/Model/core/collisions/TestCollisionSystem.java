7c03bcea87bf16e17c084e5f16b2056695c937c8
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-02-06 16:03:42 +0100

    test: implemented TestCollisionSystem

--- src/test/java/it/unibo/crabinv/Model/core/collisions/TestCollisionSystem.java
+++ src/test/java/it/unibo/crabinv/Model/core/collisions/TestCollisionSystem.java
@@ -1,5 +1,116 @@
 package it.unibo.crabinv.Model.core.collisions;
 
-public class TestCollisionSystem {
+import it.unibo.crabinv.Model.entities.bullets.BulletEnemy;
+import it.unibo.crabinv.Model.entities.bullets.BulletPlayer;
+import it.unibo.crabinv.Model.entities.enemies.EnemyImpl;
+import it.unibo.crabinv.Model.entities.entity.Entity;
+import it.unibo.crabinv.Model.entities.player.Player;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+public class TestCollisionSystem {
+    CollisionSystem collisionSystem = new CollisionSystem();
+    private static class entitiesExamples {
+        public static EnemyImpl getEnemyExample() {
+            return EnemyImpl.builder()
+                    .x(0)
+                    .y(0)
+                    .collisionGroup(CollisionGroups.HOSTILE)
+                    .radius(10)
+                    .maxHealth(1)
+                    .health(1)
+                    .build();
+        }
+        public static Player getPlayerExample() {
+            return Player.builder()
+                    .x(0)
+                    .y(0)
+                    .collisionGroup(CollisionGroups.FRIENDLY)
+                    .radius(10)
+                    .maxHealth(1)
+                    .health(1)
+                    .build();
+        }
+        public static BulletPlayer getPlayerBulletExample() {
+            return BulletPlayer.builder()
+                    .x(0)
+                    .y(0)
+                    .radius(10)
+                    .collisionGroup(CollisionGroups.FRIENDLY)
+                    .maxHealth(1)
+                    .health(1)
+                    .build();
+        }
+        public static BulletEnemy getEnemyBulletExample() {
+            return BulletEnemy.builder()
+                    .x(0)
+                    .y(0)
+                    .radius(10)
+                    .collisionGroup(CollisionGroups.HOSTILE)
+                    .maxHealth(1)
+                    .health(1)
+                    .build();
+        }
+        public static EnemyImpl getAlreadyDeadEnemyExample() {
+            return EnemyImpl.builder()
+                    .x(0)
+                    .y(0)
+                    .radius(10)
+                    .collisionGroup(CollisionGroups.HOSTILE)
+                    .maxHealth(1)
+                    .health(0)
+                    .build();
+        }
+    }
+
+    @Test
+    public void testCollisionBetweenLiveEnemies() {
+        Entity enemy = entitiesExamples.getEnemyExample();
+        Entity player = entitiesExamples.getPlayerExample();
+        List<Entity> entities = new ArrayList<>();
+        entities.add(enemy);
+        entities.add(player);
+        Assertions.assertTrue(collisionSystem.resolve(entities));
+        Assertions.assertFalse(enemy.isAlive());
+        Assertions.assertFalse(player.isAlive());
+    }
+
+    @Test
+    public void testCollisionBetweenFriendlies() {
+        Entity bullet = entitiesExamples.getPlayerBulletExample();
+        Entity player = entitiesExamples.getPlayerExample();
+        List<Entity> entities = new ArrayList<>();
+        entities.add(bullet);
+        entities.add(player);
+        Assertions.assertFalse(collisionSystem.resolve(entities));
+        Assertions.assertTrue(bullet.isAlive());
+        Assertions.assertTrue(player.isAlive());
+    }
+
+    @Test
+    public void testCollisionBetweenEnemies() {
+        Entity bullet = entitiesExamples.getEnemyBulletExample();
+        Entity enemy = entitiesExamples.getEnemyExample();
+        List<Entity> entities = new ArrayList<>();
+        entities.add(bullet);
+        entities.add(enemy);
+        Assertions.assertFalse(collisionSystem.resolve(entities));
+        Assertions.assertTrue(bullet.isAlive());
+        Assertions.assertTrue(enemy.isAlive());
+    }
+
+    @Test
+    public void testCollisionWhereOneEntityIsDead() {
+        Entity enemy = entitiesExamples.getAlreadyDeadEnemyExample();
+        Entity player = entitiesExamples.getPlayerExample();
+        List<Entity> entities = new ArrayList<>();
+        entities.add(enemy);
+        entities.add(player);
+        Assertions.assertFalse(collisionSystem.resolve(entities));
+        Assertions.assertFalse(enemy.isAlive());
+        Assertions.assertTrue(player.isAlive());
+    }
 }
