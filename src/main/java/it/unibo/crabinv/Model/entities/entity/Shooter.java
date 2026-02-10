0a341214dc911340766ad8c345013b6f2ffd0e5e
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-02-10 17:57:26 +0100

    refactor: corrected style violations in Shooter

--- src/main/java/it/unibo/crabinv/Model/entities/entity/Shooter.java
+++ src/main/java/it/unibo/crabinv/Model/entities/entity/Shooter.java
@@ -1,23 +1,25 @@
 package it.unibo.crabinv.Model.entities.entity;
 
 /**
- * Provides the basic methods that a shooter entity should implement
+ * Provides the basic methods that a shooter entity should implement.
  */
 public interface Shooter {
     /**
-     * Checks if the entity is currently able to shoot
+     * Checks if the entity is currently able to shoot.
+     *
      * @return true if it can shoot, false if it can't
      */
-    public boolean isAbleToShoot();
+    boolean isAbleToShoot();
 
     /**
-     * Returns the fireRate of the entity
+     * Returns the fireRate of the entity.
+     *
      * @return the amount of ticks needed between two shots
      */
-    public int getFireRate();
+    int getFireRate();
 
     /**
-     * Tells the model that it has shot, prompting it to handle the isAbleToShoot pipeline
+     * Tells the model that it has shot, prompting it to handle the isAbleToShoot pipeline.
      */
-    public void shoot();
+    void shoot();
 }
