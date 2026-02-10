850b177d6b253ea5cb621343af54aadb92cf2377
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-02-10 18:50:21 +0100

    fix: fixed the test of the shop

--- src/test/java/it/unibo/crabinv/Model/powerUpsShop/TestShop.java
+++ src/test/java/it/unibo/crabinv/Model/powerUpsShop/TestShop.java
@@ -1,56 +1,61 @@
 package it.unibo.crabinv.Model.powerUpsShop;
+
 import it.unibo.crabinv.Model.save.UserProfile;
 import it.unibo.crabinv.Model.save.UserProfileImpl;
 import org.junit.jupiter.api.Test;
-
 import java.util.LinkedHashMap;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import static org.junit.jupiter.api.Assertions.*;
+class TestShop {
+    private static final int MAGIC_NUMBER_20 = 20;
+    private static final int MAGIC_NUMBER_50 = 50;
 
-public class TestShop {
     @Test
     public void testSuccefulPurchase() {
-        Shop shop = new ShopLogic();
-        UserProfile profile = new UserProfileImpl(new LinkedHashMap<>());
-        PowerUp powerUp = new PowerUpLogic(PowerUpType.HEALTH_UP,50, 3);
+        final Shop shop = new ShopLogic();
+        final UserProfile profile = new UserProfileImpl(new LinkedHashMap<>());
+        final PowerUp powerUp = new PowerUpLogic(PowerUpType.HEALTH_UP, MAGIC_NUMBER_50, 3);
 
         profile.addCurrency(100);
-        boolean result = shop.purchase(profile,powerUp);
+        final boolean result = shop.purchase(profile, powerUp);
 
         assertTrue(result);
-        assertEquals(50,profile.getCurrency());
+        assertEquals(MAGIC_NUMBER_50, profile.getCurrency());
         assertEquals(2, profile.getPowerUpLevel(powerUp.getPowerUpType()));
 
     }
 
     @Test
     public void testFailPurchaseForCurrency() {
-        Shop shop = new ShopLogic();
-        UserProfile profile = new UserProfileImpl(new LinkedHashMap<>());
-        PowerUp powerUp = new PowerUpLogic(PowerUpType.HEALTH_UP,50, 3);
+        final Shop shop = new ShopLogic();
+        final UserProfile profile = new UserProfileImpl(new LinkedHashMap<>());
+        final PowerUp powerUp = new PowerUpLogic(PowerUpType.HEALTH_UP, MAGIC_NUMBER_50, 3);
 
-        profile.addCurrency(20);
-        boolean result = shop.purchase(profile,powerUp);
+        profile.addCurrency(MAGIC_NUMBER_20);
+        final boolean result = shop.purchase(profile, powerUp);
 
         assertFalse(result);
 
-        assertEquals(20,profile.getCurrency());
+        assertEquals(MAGIC_NUMBER_20, profile.getCurrency());
         assertEquals(1, profile.getPowerUpLevel(powerUp.getPowerUpType()));
 
     }
+
     @Test
     public void testFailPurchaseForMaxLevel() {
-        Shop shop = new ShopLogic();
-        UserProfile profile = new UserProfileImpl(new LinkedHashMap<>());
-        PowerUp powerUp = new PowerUpLogic(PowerUpType.HEALTH_UP,50, 3);
+        final Shop shop = new ShopLogic();
+        final UserProfile profile = new UserProfileImpl(new LinkedHashMap<>());
+        final PowerUp powerUp = new PowerUpLogic(PowerUpType.HEALTH_UP, MAGIC_NUMBER_50, 3);
 
         profile.addCurrency(100);
-        profile.updatePowerUp(powerUp.getPowerUpType(),3);
+        profile.updatePowerUp(powerUp.getPowerUpType(), 3);
 
-        boolean result = shop.purchase(profile,powerUp);
+        final boolean result = shop.purchase(profile, powerUp);
 
         assertFalse(result);
-        assertEquals(100,profile.getCurrency());
+        assertEquals(100, profile.getCurrency());
         assertEquals(3, profile.getPowerUpLevel(powerUp.getPowerUpType()));
     }
 }
