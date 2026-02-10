b1ed27b804a8414675fa1bcc5a7b2f0cdb7d719a
Mosè Barbieri <mose.barbieri@studio.unibo.it>
2026-02-10 18:16:21 +0100

    fix: fixed the styles of the files of the bullets

--- src/main/java/it/unibo/crabinv/Model/entities/bullets/Bullet.java
+++ src/main/java/it/unibo/crabinv/Model/entities/bullets/Bullet.java
@@ -5,11 +5,12 @@ import it.unibo.crabinv.Model.entities.entity.EntitySprites;
 import it.unibo.crabinv.Model.entities.entity.Movable;
 
 /**
- * Provides all bullet implementations of all the methods they should implement by combining Entity and Movable
+ * Provides all bullet implementations of all the methods they should implement by combining Entity and Movable.
  */
 public interface Bullet extends Entity, Movable {
     /**
-     * Getter of the sprite
+     * Getter of the sprite.
+     *
      * @return the image path to the sprite
      */
     EntitySprites getSprites();
