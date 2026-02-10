ed45c56dc912f4361ada4c935acb6d88cb4ccaf7
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-02-10 18:57:44 +0100

    feat: added ViewParameters

---
+++ src/main/java/it/unibo/crabinv/View/ViewParameters.java
@@ -0,0 +1,8 @@
+package it.unibo.crabinv.View;
+
+/**
+ * Provides the default parameters for the views
+ */
+public final class ViewParameters {
+    public static final int DEFAULT_SPACING = 20;
+}
