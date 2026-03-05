f0241c3c05a89c43191e9ed3f820eeb0337a1bc6
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-05 16:37:32 +0100

    refactor: converted TestInputRegistration.java to kotlin

--- src/test/java/it/unibo/crabinv/controller/core/input/TestInputRegistration.kt
+++ src/test/java/it/unibo/crabinv/controller/core/input/TestInputRegistration.kt
@@ -1,24 +1,23 @@
-package it.unibo.crabinv.controller.core.input;
+package it.unibo.crabinv.controller.core.input
 
-import it.unibo.crabinv.model.entities.entity.Delta;
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import it.unibo.crabinv.model.entities.entity.Delta
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 
 /**
  * Tests if the registration of the input works correctly.
  */
-class TestInputRegistration {
-
+internal class TestInputRegistration {
     @Test
-    void pressingRightRegistersIncreaseAndReleaseClearsIt() {
-        final InputController controller = new InputControllerPlayer(new InputMapperImpl());
+    fun pressingRightRegistersIncreaseAndReleaseClearsIt() {
+        val controller: InputController = InputControllerPlayer(InputMapperImpl())
 
-        Assertions.assertEquals(Delta.NO_ACTION, controller.getInputState().getXMovementDelta());
+        Assertions.assertEquals(Delta.NO_ACTION, controller.getInputState().getXMovementDelta())
 
-        controller.onKeyPressed(KeyCodeKeyboard.RIGHT.getKeyCode());
-        Assertions.assertEquals(Delta.INCREASE, controller.getInputState().getXMovementDelta());
+        controller.onKeyPressed(KeyCodeKeyboard.RIGHT.keyCode)
+        Assertions.assertEquals(Delta.INCREASE, controller.getInputState().getXMovementDelta())
 
-        controller.onKeyReleased(KeyCodeKeyboard.RIGHT.getKeyCode());
-        Assertions.assertEquals(Delta.NO_ACTION, controller.getInputState().getXMovementDelta());
+        controller.onKeyReleased(KeyCodeKeyboard.RIGHT.keyCode)
+        Assertions.assertEquals(Delta.NO_ACTION, controller.getInputState().getXMovementDelta())
     }
 }
