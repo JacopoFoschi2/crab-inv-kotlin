92a7535a2210d311bf56b2d7d37fa363c4ab17d8
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-05 16:36:31 +0100

    refactor: converted GameSessionTest.java to kotlin

--- src/test/java/it/unibo/crabinv/core/save/GameSessionTest.kt
+++ src/test/java/it/unibo/crabinv/core/save/GameSessionTest.kt
@@ -1,52 +1,50 @@
-package it.unibo.crabinv.core.save;
+package it.unibo.crabinv.core.save
 
-import it.unibo.crabinv.model.core.save.GameSessionImpl;
-import it.unibo.crabinv.model.core.save.PlayerBaseStats;
-import it.unibo.crabinv.model.core.save.StartingSaveValues;
-import it.unibo.crabinv.model.powerups.PowerUpType;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import it.unibo.crabinv.model.core.save.GameSessionImpl
+import it.unibo.crabinv.model.core.save.PlayerBaseStats
+import it.unibo.crabinv.model.core.save.StartingSaveValues
+import it.unibo.crabinv.model.powerups.PowerUpType
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
 /**
  * Tests the game session.
  */
-class GameSessionTest {
-
-    private GameSessionImpl session;
+internal class GameSessionTest {
+    private var session: GameSessionImpl? = null
 
     @BeforeEach
-    void init() {
-        session = new GameSessionImpl(
-                StartingSaveValues.CURRENCY.getIntValue(),
-                PlayerBaseStats.getIntValueOf(PowerUpType.HEALTH_UP),
+    fun init() {
+        session =
+            GameSessionImpl(
+                StartingSaveValues.CURRENCY.intValue,
+                PlayerBaseStats.getIntValueOf(PowerUpType.HEALTH_UP).toDouble(),
                 PlayerBaseStats.getDoubleValueOf(PowerUpType.SPEED_UP),
-                PlayerBaseStats.getIntValueOf(PowerUpType.FIRERATE_UP)
-                );
+                PlayerBaseStats.getIntValueOf(PowerUpType.FIRERATE_UP).toDouble(),
+            )
     }
 
     @Test
-    void testInitialState() {
-        final int addCurrency = 50;
-        final int subCurrency = 80;
-        final int subHealth = 5;
-        assertEquals(1, session.getCurrentLevel());
-        assertEquals(0, session.getCurrency());
-        session.addCurrency(addCurrency);
-        assertEquals(addCurrency, session.getCurrency());
-        assertThrows(IllegalArgumentException.class, () -> session.subCurrency(subCurrency));
-        assertEquals(PlayerBaseStats.getIntValueOf(PowerUpType.HEALTH_UP), session.getPlayerHealth());
-        session.subPlayerHealth(subHealth);
-        assertEquals(0, session.getPlayerHealth());
-        session.advanceLevel();
-        assertEquals(2, session.getCurrentLevel());
-        assertEquals(3, session.getNextLevel());
-        assertFalse(session.isGameOver());
-        session.markGameOver();
-        assertTrue(session.isGameOver());
+    fun testInitialState() {
+        val addCurrency = 50
+        val subCurrency = 80
+        val subHealth = 5
+        Assertions.assertEquals(1, session!!.currentLevel)
+        Assertions.assertEquals(0, session!!.currency)
+        session!!.addCurrency(addCurrency)
+        Assertions.assertEquals(addCurrency, session!!.currency)
+        Assertions.assertThrows(
+            IllegalArgumentException::class.java,
+        ) { session!!.subCurrency(subCurrency) }
+        Assertions.assertEquals(PlayerBaseStats.getIntValueOf(PowerUpType.HEALTH_UP), session!!.playerHealth)
+        session!!.subPlayerHealth(subHealth)
+        Assertions.assertEquals(0, session!!.playerHealth)
+        session!!.advanceLevel()
+        Assertions.assertEquals(2, session!!.currentLevel)
+        Assertions.assertEquals(3, session!!.nextLevel)
+        Assertions.assertFalse(session!!.isGameOver)
+        session!!.markGameOver()
+        Assertions.assertTrue(session!!.isGameOver)
     }
 }
