f6a38bbd621a0a103f459fea63531be1aa415c55
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-05 15:51:30 +0100

    refactor: converted TestJavaFXSoundManager.java to kotlin

--- src/test/java/it/unibo/crabinv/model/core/audio/TestJavaFXSoundManager.kt
+++ src/test/java/it/unibo/crabinv/model/core/audio/TestJavaFXSoundManager.kt
@@ -1,38 +1,41 @@
-package it.unibo.crabinv.model.core.audio;
+package it.unibo.crabinv.model.core.audio
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
 
-class TestJavaFXSoundManager {
-    private static final double DEFAULT_VOLUME = 1.0;
-    private static final double TEST_VOLUME = 0.5;
-    private final SoundService soundManager = new JavaFXSoundManager();
+internal class TestJavaFXSoundManager {
+    private val soundManager: SoundService = JavaFXSoundManager()
 
     @Test
-    void testBGMMute() {
-        Assertions.assertFalse(soundManager.isBGMMuted());
-        soundManager.toggleMuteBGM();
-        Assertions.assertTrue(soundManager.isBGMMuted());
+    fun testBGMMute() {
+        Assertions.assertFalse(soundManager.isBGMMuted())
+        soundManager.toggleMuteBGM()
+        Assertions.assertTrue(soundManager.isBGMMuted())
     }
 
     @Test
-    void testSFXMute() {
-        Assertions.assertFalse(soundManager.isSFXMuted());
-        soundManager.toggleMuteSFX();
-        Assertions.assertTrue(soundManager.isSFXMuted());
+    fun testSFXMute() {
+        Assertions.assertFalse(soundManager.isSFXMuted())
+        soundManager.toggleMuteSFX()
+        Assertions.assertTrue(soundManager.isSFXMuted())
     }
 
     @Test
-    void testBGMVolumeChange() {
-        Assertions.assertEquals(DEFAULT_VOLUME, soundManager.getBGMVolume());
-        soundManager.setBGMVolume(TEST_VOLUME);
-        Assertions.assertEquals(TEST_VOLUME, soundManager.getBGMVolume());
+    fun testBGMVolumeChange() {
+        Assertions.assertEquals(DEFAULT_VOLUME, soundManager.getBGMVolume())
+        soundManager.setBGMVolume(TEST_VOLUME)
+        Assertions.assertEquals(TEST_VOLUME, soundManager.getBGMVolume())
     }
 
     @Test
-    void testSFXVolumeChange() {
-        Assertions.assertEquals(DEFAULT_VOLUME, soundManager.getSFXVolume());
-        soundManager.setSFXVolume(TEST_VOLUME);
-        Assertions.assertEquals(TEST_VOLUME, soundManager.getSFXVolume());
+    fun testSFXVolumeChange() {
+        Assertions.assertEquals(DEFAULT_VOLUME, soundManager.getSFXVolume())
+        soundManager.setSFXVolume(TEST_VOLUME)
+        Assertions.assertEquals(TEST_VOLUME, soundManager.getSFXVolume())
+    }
+
+    companion object {
+        private const val DEFAULT_VOLUME = 1.0
+        private const val TEST_VOLUME = 0.5
     }
 }
