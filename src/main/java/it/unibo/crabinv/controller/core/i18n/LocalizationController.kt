a981676c462b3a134d6d992896e3a4ab2e821fd6
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-05 15:40:49 +0100

    refactor: converted LocalizationController.java to kotlin

--- src/main/java/it/unibo/crabinv/controller/core/i18n/LocalizationController.kt
+++ src/main/java/it/unibo/crabinv/controller/core/i18n/LocalizationController.kt
@@ -1,47 +1,34 @@
-package it.unibo.crabinv.controller.core.i18n;
+package it.unibo.crabinv.controller.core.i18n
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import it.unibo.crabinv.model.core.i18n.Localization;
-import it.unibo.crabinv.model.core.i18n.SupportedLocales;
-import it.unibo.crabinv.model.core.i18n.TextKeys;
+import it.unibo.crabinv.model.core.i18n.Localization
+import it.unibo.crabinv.model.core.i18n.SupportedLocales
+import it.unibo.crabinv.model.core.i18n.TextKeys
 
 /**
  * Provides all the apis to control the localization.
+ * Binds the controller to it's model [Localization].
+ *
+ * @param loc the injected instance of localization
  */
-public class LocalizationController {
-    private final Localization loc;
+class LocalizationController(
+    private val loc: Localization,
+) {
+    var language: SupportedLocales?
+        /**
+         * @return the currently selected Locale
+         */
+        get() = loc.getCurrentLocale()
 
-    /**
-     * Binds the controller to it's model {@link Localization}.
-     *
-     * @param loc the injected instance of localization
-     */
-    @SuppressFBWarnings("EI_EXPOSE_REP2") // Localization is injected and owned by the caller.
-    public LocalizationController(final Localization loc) {
-        this.loc = loc;
-    }
-
-    /**
-     * Sets language.
-     *
-     * @param locale the language to set
-     */
-    public void setLanguage(final SupportedLocales locale) {
-        loc.setLocale(locale);
-    }
-
-    /**
-     * @return the currently selected Locale
-     */
-    public SupportedLocales getLanguage() {
-        return loc.getCurrentLocale();
-    }
+        /**
+         * Sets language.
+         *
+         * @param locale the language to set
+         */
+        set(locale) = loc.setLocale(locale)
 
     /**
      * @param key the key you want to take the string from
      * @return the string associated to the key in input in the set language
      */
-    public String getString(final TextKeys key) {
-        return loc.getString(key);
-    }
+    fun getString(key: TextKeys): String = loc.getString(key)
 }
