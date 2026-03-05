c529308616ba4c719b8257452e893a094b9e8431
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-05 15:40:28 +0100

    refactor: converted SupportedLocales.java to kotlin

--- src/main/java/it/unibo/crabinv/model/core/i18n/SupportedLocales.kt
+++ src/main/java/it/unibo/crabinv/model/core/i18n/SupportedLocales.kt
@@ -1,55 +1,44 @@
-package it.unibo.crabinv.model.core.i18n;
+package it.unibo.crabinv.model.core.i18n
 
-import java.util.Locale;
+import java.util.Locale
 
 /**
  * Provides all currently supported locales to ensure correct usage.
  * It also lists the path of the flag representing said language, and it's localised name
  */
-public enum SupportedLocales {
-    ENGLISH(new Locale.Builder().setLanguage("en").setRegion("US").build(), "/flags/flag_en.png", "ENGLISH"),
-    ITALIAN(new Locale.Builder().setLanguage("it").setRegion("IT").build(), "/flags/flag_it.png", "ITALIANO"),;
-
-    private final Locale locale;
-    private final String imagePath;
-    private final String localizedName;
-
-    /**
-     * Constructs the enum of all the languages.
-     *
-     * @param locale the set locale
-     * @param imagePath the imagePath of the flag that represents it
-     * @param localizedName the localized name of the language
-     */
-    SupportedLocales(final Locale locale, final String imagePath, final String localizedName) {
-        this.locale = locale;
-        this.imagePath = imagePath;
-        this.localizedName = localizedName;
-    }
-
+enum class SupportedLocales(
     /**
      * @return the locale
      */
-    public Locale getLocale() {
-        return locale;
-    }
-
+    val locale: Locale,
     /**
      * @return the image path of the language flag
      */
-    public String getImagePath() {
-        return imagePath;
-    }
-
+    val imagePath: String,
     /**
      * @return the localized name of the language
      */
-    public String getLocalizedName() {
-        return localizedName;
-    }
+    val localizedName: String,
+) {
+    ENGLISH(
+        Locale
+            .Builder()
+            .setLanguage("en")
+            .setRegion("US")
+            .build(),
+        "/flags/flag_en.png",
+        "ENGLISH",
+    ),
+    ITALIAN(
+        Locale
+            .Builder()
+            .setLanguage("it")
+            .setRegion("IT")
+            .build(),
+        "/flags/flag_it.png",
+        "ITALIANO",
+    ),
+    ;
 
-    @Override
-    public String toString() {
-        return getLocalizedName();
-    }
+    override fun toString(): String = this.localizedName
 }
