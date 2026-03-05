347c471e839866193c1d46dfb192888828209767
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-05 15:47:36 +0100

    refactor: converted TestLocalization.java to kotlin

--- src/test/java/it/unibo/crabinv/model/core/i18n/TestLocalization.kt
+++ src/test/java/it/unibo/crabinv/model/core/i18n/TestLocalization.kt
@@ -1,37 +1,37 @@
-package it.unibo.crabinv.model.core.i18n;
+package it.unibo.crabinv.model.core.i18n
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
 
-class TestLocalization {
-    private Localization loc;
+internal class TestLocalization {
+    private var loc: Localization? = null
 
     @BeforeEach
-    public void setup() {
-        loc = new Localization(SupportedLocales.ENGLISH);
+    fun setup() {
+        loc = Localization(SupportedLocales.ENGLISH)
     }
 
     @Test
-    void testCorrectKeyFetch() {
-        final String expectedResult = "PLAY";
-        Assertions.assertEquals(expectedResult, loc.getString(TextKeys.PLAY));
+    fun testCorrectKeyFetch() {
+        val expectedResult = "PLAY"
+        Assertions.assertEquals(expectedResult, loc!!.getString(TextKeys.PLAY))
     }
 
     @Test
-    void testCorrectLocaleSwap() {
-        final String expectedResult = "GIOCA";
-        loc.setLocale(SupportedLocales.ITALIAN);
-        Assertions.assertEquals(expectedResult, loc.getString(TextKeys.PLAY));
+    fun testCorrectLocaleSwap() {
+        val expectedResult = "GIOCA"
+        loc!!.setLocale(SupportedLocales.ITALIAN)
+        Assertions.assertEquals(expectedResult, loc!!.getString(TextKeys.PLAY))
     }
 
     @Test
-    void testCorrectLocaleSwapped() {
-        var expectedResult = SupportedLocales.ITALIAN;
-        loc.setLocale(SupportedLocales.ITALIAN);
-        Assertions.assertEquals(expectedResult, loc.getCurrentLocale());
-        expectedResult = SupportedLocales.ENGLISH;
-        loc.setLocale(SupportedLocales.ENGLISH);
-        Assertions.assertEquals(expectedResult, loc.getCurrentLocale());
+    fun testCorrectLocaleSwapped() {
+        var expectedResult = SupportedLocales.ITALIAN
+        loc!!.setLocale(SupportedLocales.ITALIAN)
+        Assertions.assertEquals(expectedResult, loc!!.getCurrentLocale())
+        expectedResult = SupportedLocales.ENGLISH
+        loc!!.setLocale(SupportedLocales.ENGLISH)
+        Assertions.assertEquals(expectedResult, loc!!.getCurrentLocale())
     }
 }
