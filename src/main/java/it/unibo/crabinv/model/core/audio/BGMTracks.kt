93dd4d1641394519518a4972a334758b544558b8
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-06 10:10:34 +0100

    refactor: converted BGMTracks.java to kotlin

--- src/main/java/it/unibo/crabinv/model/core/audio/BGMTracks.kt
+++ src/main/java/it/unibo/crabinv/model/core/audio/BGMTracks.kt
@@ -1,22 +1,14 @@
-package it.unibo.crabinv.model.core.audio;
+package it.unibo.crabinv.model.core.audio
 
 /**
  * Provides a set list of present BGM Tracks and their relative path in resources.
  */
-public enum BGMTracks {
-    LEVEL("/audio/BGM/main_bgm.mp3"),
-    MENU("/audio/BGM/menu_bgm.mp3"),;
-
-    private final String path;
-
-    BGMTracks(final String path) {
-        this.path = path;
-    }
-
+enum class BGMTracks(
     /**
      * @return the resource path of the selected BGM
      */
-    public String getPath() {
-        return path;
-    }
+    val path: String,
+) {
+    LEVEL("/audio/BGM/main_bgm.mp3"),
+    MENU("/audio/BGM/menu_bgm.mp3"),
 }
