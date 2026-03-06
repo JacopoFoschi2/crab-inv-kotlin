c992c2d7b886251f4eae690826162e9ab2de93f5
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-06 10:11:28 +0100

    refactor: converted SFXTracks.java to kotlin

--- src/main/java/it/unibo/crabinv/model/core/audio/SFXTracks.kt
+++ src/main/java/it/unibo/crabinv/model/core/audio/SFXTracks.kt
@@ -1,9 +1,14 @@
-package it.unibo.crabinv.model.core.audio;
+package it.unibo.crabinv.model.core.audio
 
 /**
  * Provides a set list of present SFX Tracks and their relative path in resources.
  */
-public enum SFXTracks {
+enum class SFXTracks(
+    /**
+     * @return the resource path of the selected SFX
+     */
+    val path: String,
+) {
     MENU_OPEN("/audio/SFX/menu_open.mp3"),
     MENU_CLOSE("/audio/SFX/menu_close.mp3"),
     MENU_SELECT("/audio/SFX/menu_select.mp3"),
@@ -11,18 +16,5 @@ public enum SFXTracks {
     SHOT_ENEMY("/audio/SFX/shot_enemy.mp3"),
     SHOT_PLAYER("/audio/SFX/shot_player.mp3"),
     EXPLOSION("/audio/SFX/explosion.mp3"),
-    SLIDER("/audio/SFX/slider.mp3"),;
-
-    private final String path;
-
-    SFXTracks(final String path) {
-        this.path = path;
-    }
-
-    /**
-     * @return the resource path of the selected SFX
-     */
-    public String getPath() {
-        return path;
-    }
+    SLIDER("/audio/SFX/slider.mp3"),
 }
