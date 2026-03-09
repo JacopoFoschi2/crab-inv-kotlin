4e48cb79b1dbc7c752675e7916ece40f15ac9de0
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-09 11:58:33 +0100

    refactor: converted CollisionController.java to kotlin

--- src/main/java/it/unibo/crabinv/controller/core/collision/CollisionController.kt
+++ src/main/java/it/unibo/crabinv/controller/core/collision/CollisionController.kt
@@ -1,37 +1,27 @@
-package it.unibo.crabinv.controller.core.collision;
+package it.unibo.crabinv.controller.core.collision
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import it.unibo.crabinv.controller.core.audio.AudioController;
-import it.unibo.crabinv.model.core.audio.SFXTracks;
-import it.unibo.crabinv.model.core.collisions.CollisionSystem;
-import it.unibo.crabinv.model.entities.entity.Entity;
-
-import java.util.List;
+import it.unibo.crabinv.controller.core.audio.AudioController
+import it.unibo.crabinv.model.core.audio.SFXTracks
+import it.unibo.crabinv.model.core.collisions.CollisionSystem
+import it.unibo.crabinv.model.entities.entity.Entity
 
 /**
- * Provides the method {@code resolve()} to resolve collisions across entities.
+ * Provides the method `resolve()` to resolve collisions across entities.
+ *
+ * @param audio the pre-existing instance of [AudioController]
  */
-public class CollisionController {
-    private final CollisionSystem collisionSystem;
-    private final AudioController audio;
+class CollisionController(
+    private val audio: AudioController,
+) {
+    private val collisionSystem = CollisionSystem()
 
     /**
-     * @param audioController the pre-existing instance of audioController
-     */
-    @SuppressFBWarnings("EI_EXPOSE_REP2") //dependencies are injected and owned by caller
-    public CollisionController(final AudioController audioController) {
-        this.audio = audioController;
-        this.collisionSystem = new CollisionSystem();
-    }
-
-    /**
-     * Calls the resolve method of CollisionSystem and plays sound if there has been a collision.
-     *
+     * Calls the resolve method of [CollisionSystem] and plays sound if there has been a collision.
      * @param entities the list of entities on screen
      */
-    public void resolve(final List<Entity> entities) {
+    fun resolve(entities: List<Entity>) {
         if (collisionSystem.resolve(entities)) {
-            audio.playSFX(SFXTracks.EXPLOSION);
+            audio.playSFX(SFXTracks.EXPLOSION)
         }
     }
 }
