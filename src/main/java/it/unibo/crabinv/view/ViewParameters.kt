c478cec8fdd5a798326b98af174366571ffef7d0
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-12 13:59:39 +0100

    refactor: converted ViewParameters.java to kotlin

--- src/main/java/it/unibo/crabinv/view/ViewParameters.kt
+++ src/main/java/it/unibo/crabinv/view/ViewParameters.kt
@@ -1,27 +1,22 @@
-package it.unibo.crabinv.view;
+package it.unibo.crabinv.view
 
-import javafx.geometry.Pos;
+import javafx.geometry.Pos
 
 /**
  * Provides the default parameters for the views.
  */
-public final class ViewParameters {
-    public static final int DEFAULT_SPACING_MAINMENU = 30;
-    public static final int DEFAULT_CARD_SPACING = 12;
-    public static final int DEFAULT_HEADER_SPACING = 25;
-    public static final int DEFAULT_WIDTH = 600;
-    public static final int DEFAULT_SPACING = 20;
-    public static final int DEFAULT_PAUSE_WIDTH = 400;
-    public static final int DEFAULT_SLIDER_INCREMENTS = 5;
-    public static final int DEFAULT_INSETS_DESCRIPTION = 15;
-    public static final int DEFAULT_INSETS_POWERUP_CARD = 15;
-    public static final int DEFAULT_INSETS_PANE = 20;
-    public static final int DEFAULT_INSETS = 30;
-    public static final Pos SETTINGS_ALIGMENT = Pos.CENTER_LEFT;
-    public static final int DEFAULT_HUD_MARGIN = 10;
-
-    /**
-     * Ensures the class isn't wrongly instantiated.
-     */
-    private ViewParameters() { }
+object ViewParameters {
+    const val DEFAULT_SPACING_MAINMENU: Int = 30
+    const val DEFAULT_CARD_SPACING: Int = 12
+    const val DEFAULT_HEADER_SPACING: Int = 25
+    const val DEFAULT_WIDTH: Int = 600
+    const val DEFAULT_SPACING: Int = 20
+    const val DEFAULT_PAUSE_WIDTH: Int = 400
+    const val DEFAULT_SLIDER_INCREMENTS: Int = 5
+    const val DEFAULT_INSETS_DESCRIPTION: Int = 15
+    const val DEFAULT_INSETS_POWERUP_CARD: Int = 15
+    const val DEFAULT_INSETS_PANE: Int = 20
+    const val DEFAULT_INSETS: Int = 30
+    val SETTINGS_ALIGMENT: Pos = Pos.CENTER_LEFT
+    const val DEFAULT_HUD_MARGIN: Int = 10
 }
