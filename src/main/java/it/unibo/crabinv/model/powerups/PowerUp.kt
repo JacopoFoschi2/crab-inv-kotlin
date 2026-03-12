1d71b873a8531854afac09df59d9f0c8fd4bb918
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-12 14:29:24 +0100

    refactor: converted PowerUp.java to Kotlin

--- src/main/java/it/unibo/crabinv/model/powerups/PowerUp.kt
+++ src/main/java/it/unibo/crabinv/model/powerups/PowerUp.kt
@@ -1,34 +1,26 @@
-package it.unibo.crabinv.model.powerups;
+package it.unibo.crabinv.model.powerups
 
 /**
  * It's the interface that implements the methods for the powerUp.
  */
-public interface PowerUp {
+interface PowerUp {
     /**
-     * Method to get the cost of the single powerUp.
-     *
      * @return the cost of the powerUp
      */
-    int getCost();
+    val cost: Int
 
     /**
-     * Method to get to the maxLevel of the powerUp.
-     *
      * @return the max level of the powerUp
      */
-    int getMaxLevel();
+    val maxLevel: Int
 
     /**
-     * Method to get the powerUp's name.
-     *
      * @return the powerUp name
      */
-    String getPowerUpName();
+    val powerUpName: String
 
     /**
-     * Gets the type of the type of the power up.
-     *
      * @return the type of the powerUp
      */
-    PowerUpType getPowerUpType();
+    val powerUpType: PowerUpType
 }
