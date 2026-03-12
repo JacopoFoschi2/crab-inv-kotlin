abad7c71d63fe0749f30ce30a94cc42ac3f15b65
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-12 13:39:03 +0100

    refactor: converted PauseMenu.java to kotlin

--- src/main/java/it/unibo/crabinv/view/PauseMenu.kt
+++ src/main/java/it/unibo/crabinv/view/PauseMenu.kt
@@ -1,113 +1,126 @@
-package it.unibo.crabinv.view;
+package it.unibo.crabinv.view
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import it.unibo.crabinv.controller.core.audio.AudioController;
-import it.unibo.crabinv.controller.core.i18n.LocalizationController;
-import it.unibo.crabinv.model.core.audio.SFXTracks;
-import it.unibo.crabinv.model.core.i18n.TextKeys;
-import it.unibo.crabinv.SceneManager;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.CheckBox;
-import javafx.scene.control.Label;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.scene.layout.VBox;
-
-import static it.unibo.crabinv.view.ViewParameters.DEFAULT_PAUSE_WIDTH;
-import static it.unibo.crabinv.view.ViewParameters.DEFAULT_SPACING;
-import static it.unibo.crabinv.view.ViewParameters.SETTINGS_ALIGMENT;
+import it.unibo.crabinv.SceneManager
+import it.unibo.crabinv.controller.core.audio.AudioController
+import it.unibo.crabinv.controller.core.i18n.LocalizationController
+import it.unibo.crabinv.model.core.audio.SFXTracks
+import it.unibo.crabinv.model.core.i18n.TextKeys
+import javafx.beans.Observable
+import javafx.event.ActionEvent
+import javafx.event.EventHandler
+import javafx.geometry.Pos
+import javafx.scene.control.Button
+import javafx.scene.control.Label
+import javafx.scene.layout.HBox
+import javafx.scene.layout.Pane
+import javafx.scene.layout.StackPane
+import javafx.scene.layout.VBox
 
 /**
  * Provides the method to create the pause menu GUI.
+ * @param loc the instance of LocalizationController
+ * @param audio the instance of AudioController
+ * @param resumeMethod the method that calls the resume of the game engine
+ * @param gameOver the method that calls the gameOver of the game engine
  */
-public class PauseMenu {
-    private final SceneManager sceneManager;
-    private final LocalizationController loc;
-    private final AudioController audio;
-    private final Runnable resumeMethod;
-    private final Runnable gameOver;
-    private final SettingComponentsGenerator components;
+class PauseMenu(
+    private val sceneManager: SceneManager,
+    private val loc: LocalizationController,
+    private val audio: AudioController,
+    private val resumeMethod: Runnable,
+    private val gameOver: Runnable,
+) {
+    private val components: SettingsComponentsGenerator =
+        SettingsComponentsGenerator(sceneManager, loc, audio, ViewParameters.SETTINGS_ALIGMENT)
 
-    /**
-     * @param sceneManager the instance of SceneManager
-     * @param loc the instance of LocalizationController
-     * @param audio the instance of AudioController
-     * @param resumeMethod the method that calls the resume of the game engine
-     * @param gameOver the method that calls the gameOver of the game engine
-     */
-    @SuppressFBWarnings("EI_EXPOSE_REP2") //dependencies are injected and owned by caller
-    public PauseMenu(
-            final SceneManager sceneManager,
-            final LocalizationController loc,
-            final AudioController audio,
-            final Runnable resumeMethod,
-            final Runnable gameOver) {
-        this.sceneManager = sceneManager;
-        this.loc = loc;
-        this.audio = audio;
-        this.resumeMethod = resumeMethod;
-        this.gameOver = gameOver;
-        components = new SettingComponentsGenerator(sceneManager, loc, audio, SETTINGS_ALIGMENT);
-    }
-
-    /**
-     * @return the Stackpane containing a working pauseMenu
-     */
-    public Pane getView() {
-        final StackPane pane = new StackPane();
-        pane.getStyleClass().add("pause-pane");
-        final VBox content = new VBox(DEFAULT_SPACING);
-        content.setAlignment(Pos.CENTER);
-        content.setMaxWidth(DEFAULT_PAUSE_WIDTH);
-        final Label title = new Label(loc.getString(TextKeys.PAUSE));
-        title.getStyleClass().add("title");
-        final VBox mainColumn = new VBox(DEFAULT_SPACING);
-        mainColumn.setAlignment(SETTINGS_ALIGMENT);
-        final HBox bgmVolume = components.createVolumeSlider(
-                audio.getBgmVolume(),
-                audio::setBgmVolume,
-                audio::playSFX,
-                TextKeys.BGM_VOLUME);
-        final HBox sfxVolume = components.createVolumeSlider(
-                audio.getSfxVolume(),
-                audio::setSfxVolume,
-                audio::playSFX,
-                TextKeys.SFX_VOLUME);
-        final CheckBox bgmMute = components.createMute(TextKeys.BGM_MUTE, audio.isBGMMuted(), audio::toggleBGMMute);
-        final CheckBox sfxMute = components.createMute(TextKeys.SFX_MUTE, audio.isSFXMuted(), audio::toggleSFXMute);
-        mainColumn.getChildren().addAll(bgmVolume, sfxVolume, bgmMute, sfxMute);
-        for (final var child : mainColumn.getChildren()) {
-            child.focusedProperty().addListener(_ ->
-                    audio.playSFX(SFXTracks.MENU_HOVER));
+    val view: Pane
+        /**
+         * @return the Stackpane containing a working pauseMenu
+         */
+        get() {
+            val pane = StackPane()
+            pane.styleClass.add("pause-pane")
+            val content = VBox(ViewParameters.DEFAULT_SPACING.toDouble())
+            content.alignment = Pos.CENTER
+            content.maxWidth = ViewParameters.DEFAULT_PAUSE_WIDTH.toDouble()
+            val title = Label(loc.getString(TextKeys.PAUSE))
+            title.styleClass.add("title")
+            val mainColumn =
+                VBox(ViewParameters.DEFAULT_SPACING.toDouble())
+            mainColumn.alignment = ViewParameters.SETTINGS_ALIGMENT
+            val bgmVolume =
+                components.createVolumeSlider(
+                    audio.bgmVolume,
+                    { volume: Int -> audio.bgmVolume = volume },
+                    { sfx: SFXTracks? -> audio.playSFX(sfx!!) },
+                    TextKeys.BGM_VOLUME,
+                )
+            val sfxVolume =
+                components.createVolumeSlider(
+                    audio.sfxVolume,
+                    { volume: Int -> audio.sfxVolume = volume },
+                    { sfx: SFXTracks? -> audio.playSFX(sfx!!) },
+                    TextKeys.SFX_VOLUME,
+                )
+            val bgmMute =
+                components.createMute(
+                    TextKeys.BGM_MUTE,
+                    audio.isBGMMuted,
+                ) { audio.toggleBGMMute() }
+            val sfxMute =
+                components.createMute(
+                    TextKeys.SFX_MUTE,
+                    audio.isSFXMuted,
+                ) { audio.toggleSFXMute() }
+            mainColumn.children.addAll(bgmVolume, sfxVolume, bgmMute, sfxMute)
+            for (child in mainColumn.children) {
+                child
+                    .focusedProperty()
+                    .addListener { `_`: Observable? ->
+                        audio.playSFX(SFXTracks.MENU_HOVER)
+                    }
+            }
+            val resume =
+                createPauseMenuButton(
+                    loc.getString(TextKeys.RESUME),
+                    resumeMethod,
+                ) { sceneManager.hidePauseMenu() }
+            val exit =
+                createPauseMenuButton(
+                    loc.getString(TextKeys.ABANDON),
+                    gameOver,
+                ) { sceneManager.showMainMenu() }
+            val buttons =
+                HBox(ViewParameters.DEFAULT_SPACING.toDouble(), resume, exit)
+            buttons.alignment = Pos.CENTER
+            content.children.addAll(title, mainColumn, buttons)
+            pane.children.add(content)
+            return pane
         }
-        final Button resume = createPauseMenuButton(loc.getString(TextKeys.RESUME), resumeMethod, sceneManager::hidePauseMenu);
-        final Button exit = createPauseMenuButton(loc.getString(TextKeys.ABANDON), gameOver, sceneManager::showMainMenu);
-        final HBox buttons = new HBox(DEFAULT_SPACING, resume, exit);
-        buttons.setAlignment(Pos.CENTER);
-        content.getChildren().addAll(title, mainColumn, buttons);
-        pane.getChildren().add(content);
-        return pane;
-    }
 
     /**
      * Creates the buttons for the pause menu.
-     *
      * @param text the text of the button
      * @param method the method it's linked to
      * @param changeScene the method to change the scene
      * @return the complete button
      */
-    private Button createPauseMenuButton(final String text, final Runnable method, final Runnable changeScene) {
-        final Button button = new Button(text);
-        button.getStyleClass().add("app-button");
-        button.setOnAction(_ -> {
-            method.run();
-            changeScene.run();
-            audio.playSFX(SFXTracks.MENU_SELECT);
-        });
-        button.focusedProperty().addListener(_ -> audio.playSFX(SFXTracks.MENU_HOVER));
-        return button;
+    private fun createPauseMenuButton(
+        text: String?,
+        method: Runnable,
+        changeScene: Runnable,
+    ): Button {
+        val button = Button(text)
+        button.styleClass.add("app-button")
+        button.onAction =
+            EventHandler { `_`: ActionEvent? ->
+                method.run()
+                changeScene.run()
+                audio.playSFX(SFXTracks.MENU_SELECT)
+            }
+        button
+            .focusedProperty()
+            .addListener { `_`: Observable? -> audio.playSFX(SFXTracks.MENU_HOVER) }
+        return button
     }
 }
