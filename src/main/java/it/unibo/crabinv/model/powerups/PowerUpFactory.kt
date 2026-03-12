4741bdc39e8467c4244884d7dec85ef46541ea73
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-12 14:52:48 +0100

    refactor: converted PowerUpFactory.java to Kotlin

--- src/main/java/it/unibo/crabinv/model/powerups/PowerUpFactory.kt
+++ src/main/java/it/unibo/crabinv/model/powerups/PowerUpFactory.kt
@@ -1,35 +1,24 @@
-package it.unibo.crabinv.model.powerups;
-
-import java.util.List;
+package it.unibo.crabinv.model.powerups
 
 /**
  * It's the factory of PowerUps.
  */
-public final class PowerUpFactory {
-    private static final int SPEED_COST = 150;
-    private static final int SPEED_MAXLEVEL = 3;
-    private static final int FIRERATE_COST = 100;
-    private static final int FIRERATE_MAXLEVEL = 5;
-    private static final int HEALTH_COST = 200;
-    private static final int HEALTH_MAXLEVEL = 4;
-
-    /**
-     * It's the constructor of the power up factory.
-     */
-    private PowerUpFactory() {
-
-    }
+object PowerUpFactory {
+    private const val SPEED_COST = 150
+    private const val SPEED_MAXLEVEL = 3
+    private const val FIRERATE_COST = 100
+    private const val FIRERATE_MAXLEVEL = 5
+    private const val HEALTH_COST = 200
+    private const val HEALTH_MAXLEVEL = 4
 
     /**
      * Creates the power Ups and makes them a list.
-     *
      * @return the list of avaible power ups
      */
-    public static List<PowerUp> createShopPowerUps() {
-        return List.of(
-                new PowerUpLogic(PowerUpType.SPEED_UP, SPEED_COST, SPEED_MAXLEVEL),
-                new PowerUpLogic(PowerUpType.FIRERATE_UP, FIRERATE_COST, FIRERATE_MAXLEVEL),
-                new PowerUpLogic(PowerUpType.HEALTH_UP, HEALTH_COST, HEALTH_MAXLEVEL)
-        );
-    }
+    fun createShopPowerUps(): List<PowerUp> =
+        listOf<PowerUp>(
+            PowerUpLogic(PowerUpType.SPEED_UP, SPEED_COST, SPEED_MAXLEVEL),
+            PowerUpLogic(PowerUpType.FIRERATE_UP, FIRERATE_COST, FIRERATE_MAXLEVEL),
+            PowerUpLogic(PowerUpType.HEALTH_UP, HEALTH_COST, HEALTH_MAXLEVEL),
+        )
 }
