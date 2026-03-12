8b1e3145781dcfe5814d2eb5628aa76c580bb237
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-12 14:21:49 +0100

    refactor: converted AppPaths.java to Kotlin

--- src/main/java/it/unibo/crabinv/core/config/AppPaths.kt
+++ src/main/java/it/unibo/crabinv/core/config/AppPaths.kt
@@ -1,48 +1,28 @@
-package it.unibo.crabinv.core.config;
+package it.unibo.crabinv.core.config
 
-import java.nio.file.Path;
+import java.nio.file.Path
 
 /**
  * Provides precompiled, OS independent ready to use paths.
  * These paths are used to access the game folder in the user home
  */
-public final class AppPaths {
-    private static final String GAME_DIR = ".crabinvaders";
-    private static final String SAVES_DIR = "Saves";
-    private static final String SETTINGS_FILE = "settings.json";
-    private static final Path ROOT = Path.of(System.getProperty("user.home")).resolve(GAME_DIR);
-    private static final Path SAVES = ROOT.resolve(SAVES_DIR);
-    private static final Path SETTINGS = ROOT.resolve(SETTINGS_FILE);
+object AppPaths {
+    private const val GAME_DIR = ".crabinvaders"
+    private const val SAVES_DIR = "Saves"
+    private const val SETTINGS_FILE = "settings.json"
 
     /**
-     * Private constructor to ensure correct initialization.
-     */
-    private AppPaths() { }
-
-    /**
-     * Returns the path of the game root folder in user home.
-     *
      * @return the path to root
      */
-    public static Path getRoot() {
-        return ROOT;
-    }
+    val root: Path = Path.of(System.getProperty("user.home")).resolve(GAME_DIR)
 
     /**
-     * Returns the path of the Saves folder.
-     *
      * @return the path to the saves folder
      */
-    public static Path getSaves() {
-        return SAVES;
-    }
+    val saves: Path = root.resolve(SAVES_DIR)
 
     /**
-     * Returns the path of settings.json file.
-     *
      * @return the path to the settings.json file
      */
-    public static Path getSettings() {
-        return SETTINGS;
-    }
+    val settings: Path = root.resolve(SETTINGS_FILE)
 }
