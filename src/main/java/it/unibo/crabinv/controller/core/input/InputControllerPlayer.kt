45ad29a5ccbe7d10be9f00cf0d14ffd10e84ba94
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-13 10:33:25 +0100

    refactor: converted InputControllerPlayer.java to Kotlin

--- src/main/java/it/unibo/crabinv/controller/core/input/InputControllerPlayer.kt
+++ src/main/java/it/unibo/crabinv/controller/core/input/InputControllerPlayer.kt
@@ -1,78 +1,71 @@
-package it.unibo.crabinv.controller.core.input;
+package it.unibo.crabinv.controller.core.input
 
-import it.unibo.crabinv.model.core.input.InputSnapshot;
-import it.unibo.crabinv.model.core.input.InputSnapshotImpl;
-import it.unibo.crabinv.model.entities.entity.Delta;
-import it.unibo.crabinv.model.entities.player.Player;
-
-import java.util.HashSet;
-import java.util.Objects;
-import java.util.Set;
+import it.unibo.crabinv.model.core.input.InputSnapshot
+import it.unibo.crabinv.model.core.input.InputSnapshotImpl
+import it.unibo.crabinv.model.entities.entity.Delta
+import java.util.Objects
 
 /**
- * Implementation of the {@link InputController} for controlling the {@link Player}.
+ * Implementation of the [InputController] for controlling the [it.unibo.crabinv.model.entities.player.Player].
  */
-public class InputControllerPlayer implements InputController {
+class InputControllerPlayer(
+    mapper: InputMapper?,
+) : InputController {
+    private val pressedKeys: MutableSet<Int?> = HashSet<Int?>()
+    private val mapper: InputMapper = Objects.requireNonNull<InputMapper>(mapper)
 
-    private final Set<Integer> pressedKeys;
-    private final InputMapper mapper;
-
-    /**
-     * Constructor for the {@link InputControllerPlayer}.
-     *
-     * @param mapper the {@link InputMapper} used by the {@link InputControllerPlayer}
-     */
-    public InputControllerPlayer(final InputMapper mapper) {
-        this.mapper = Objects.requireNonNull(mapper);
-        this.pressedKeys = new HashSet<>();
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void onKeyPressed(final int keyCode) {
-        final boolean validKey =
-                mapper.mapToShoot(keyCode)
-                        || mapper.mapToXDelta(keyCode) != Delta.NO_ACTION
-                        || mapper.mapToPause(keyCode)
-                        || mapper.mapToUnPause(keyCode);
+    override fun onKeyPressed(keyCode: Int) {
+        val validKey =
+            mapper.mapToShoot(keyCode) ||
+                mapper.mapToXDelta(keyCode) != Delta.NO_ACTION ||
+                mapper.mapToPause(keyCode) ||
+                mapper.mapToUnPause(keyCode)
         if (validKey) {
-            this.pressedKeys.add(keyCode);
+            this.pressedKeys.add(keyCode)
         }
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public final void onKeyReleased(final int keyCode) {
-        this.pressedKeys.remove(keyCode);
+    override fun onKeyReleased(keyCode: Int) {
+        this.pressedKeys.remove(keyCode)
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public final InputSnapshot getInputState() {
-        final boolean isPausePressed = pressedKeys.stream()
-                .anyMatch(mapper::mapToPause);
-        final boolean isUnPausePressed = pressedKeys.stream()
-                .anyMatch(mapper::mapToUnPause);
-        final boolean isShooting = pressedKeys.stream()
-                .anyMatch(mapper::mapToShoot);
-        final boolean isInputRight = pressedKeys.stream()
-                .map(mapper::mapToXDelta)
-                .anyMatch(delta -> delta == Delta.INCREASE);
-        final boolean isInputLeft = pressedKeys.stream()
-                .map(mapper::mapToXDelta)
-                .anyMatch(delta -> delta == Delta.DECREASE);
-        Delta xMovementDelta = Delta.NO_ACTION;
-        if (isInputRight && !isInputLeft) {
-            xMovementDelta = Delta.INCREASE;
-        } else if (isInputLeft && !isInputRight) {
-            xMovementDelta = Delta.DECREASE;
+    override val inputState: InputSnapshot
+        get() {
+            val isPausePressed =
+                pressedKeys
+                    .stream()
+                    .anyMatch { inputCode: Int? -> mapper.mapToPause(inputCode!!) }
+            val isUnPausePressed =
+                pressedKeys
+                    .stream()
+                    .anyMatch { inputCode: Int? -> mapper.mapToUnPause(inputCode!!) }
+            val isShooting =
+                pressedKeys
+                    .stream()
+                    .anyMatch { inputCode: Int? -> mapper.mapToShoot(inputCode!!) }
+            val isInputRight =
+                pressedKeys
+                    .stream()
+                    .map { inputCode: Int? ->
+                        mapper.mapToXDelta(
+                            inputCode!!,
+                        )
+                    }.anyMatch { delta: Delta? -> delta == Delta.INCREASE }
+            val isInputLeft =
+                pressedKeys
+                    .stream()
+                    .map { inputCode: Int? ->
+                        mapper.mapToXDelta(
+                            inputCode!!,
+                        )
+                    }.anyMatch { delta: Delta? -> delta == Delta.DECREASE }
+            var xMovementDelta =
+                Delta.NO_ACTION
+            if (isInputRight && !isInputLeft) {
+                xMovementDelta = Delta.INCREASE
+            } else if (isInputLeft && !isInputRight) {
+                xMovementDelta = Delta.DECREASE
+            }
+            return InputSnapshotImpl(isShooting, xMovementDelta, isPausePressed, isUnPausePressed)
         }
-        return new InputSnapshotImpl(isShooting, xMovementDelta, isPausePressed, isUnPausePressed);
-    }
 }
