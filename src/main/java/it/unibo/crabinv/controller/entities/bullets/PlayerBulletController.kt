022b23811831cdca006c8d094ca204aea56e682e
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-13 10:11:16 +0100

    refactor: converted PlayerBulletController.java to kotlin

--- src/main/java/it/unibo/crabinv/controller/entities/bullets/PlayerBulletController.kt
+++ src/main/java/it/unibo/crabinv/controller/entities/bullets/PlayerBulletController.kt
@@ -1,49 +1,33 @@
-package it.unibo.crabinv.controller.entities.bullets;
+package it.unibo.crabinv.controller.entities.bullets
 
-import it.unibo.crabinv.controller.entities.entity.AbstractEntityController;
-import it.unibo.crabinv.model.entities.bullets.BulletPlayer;
-import it.unibo.crabinv.model.entities.entity.Delta;
+import it.unibo.crabinv.controller.entities.entity.AbstractEntityController
+import it.unibo.crabinv.model.entities.bullets.BulletPlayer
+import it.unibo.crabinv.model.entities.entity.Delta
 
 /**
  * It's the PlayerBulletController that handles the PlayerBullets.
+ * @param entity it's the entity needed to modify it
  */
-public final class PlayerBulletController extends AbstractEntityController<BulletPlayer> implements BulletController {
-
-    /**
-     * It's the constructor of the EnemyBulletController.
-     *
-     * @param entity it's the entity needed to modify it
-     */
-    public PlayerBulletController(final BulletPlayer entity) {
-        super(entity);
+class PlayerBulletController(
+    entity: BulletPlayer?,
+) : AbstractEntityController<BulletPlayer?>(entity),
+    BulletController {
+    override fun update(delta: Delta) {
+        move(delta)
     }
 
-    /**
-     * {@inheritDoc}
-     *
-     * @param delta either -1, 0 or 1, the former moves to the left, the latter moves to the right
-     */
-    @Override
-    public void update(final Delta delta) {
-        move(delta);
-    }
-
-    /**
-     * Gives the speed of the bullet.
-     *
-     * @return the speed of the bullet
-     */
-    public double getSpeed() {
-        return super.getEntity().getSpeed();
-    }
+    val speed: Double
+        /**
+         * Gives the speed of the bullet.
+         * @return the speed of the bullet
+         */
+        get() = super.entity!!.speed
 
     /**
      * Tells the bullet to go to a specific direction for 1 tick.
-     *
      * @param delta either -1, 0 or 1, the former moves to the left, the latter moves to the right
      */
-    private void move(final Delta delta) {
-        super.getEntity().move(delta);
+    private fun move(delta: Delta) {
+        super.entity!!.move(delta)
     }
-
 }
