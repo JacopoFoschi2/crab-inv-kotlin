67ee67aefff96b22dcb32cb14bfcc500b2ede289
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-13 10:33:38 +0100

    refactor: converted InputMapper.java to Kotlin

--- src/main/java/it/unibo/crabinv/controller/core/input/InputMapper.kt
+++ src/main/java/it/unibo/crabinv/controller/core/input/InputMapper.kt
@@ -1,41 +1,36 @@
-package it.unibo.crabinv.controller.core.input;
+package it.unibo.crabinv.controller.core.input
 
-import it.unibo.crabinv.model.entities.entity.Delta;
+import it.unibo.crabinv.model.entities.entity.Delta
 
 /**
  * Checks if the key pressed is bound to any action.
  */
-public interface InputMapper {
-
+interface InputMapper {
     /**
      * Checks if the inputCode of the key pressed is bound to X-axis movement.
-     *
      * @param inputCode of the key pressed
      * @return the X-axis movement
      */
-    Delta mapToXDelta(int inputCode);
+    fun mapToXDelta(inputCode: Int): Delta?
 
     /**
      * Checks if the inputCode of the key pressed is bound to shoot action.
-     *
      * @param inputCode of the key pressed
      * @return boolean based on the action being requested or not
      */
-    boolean mapToShoot(int inputCode);
+    fun mapToShoot(inputCode: Int): Boolean
 
     /**
      * Checks if the inputCode of the key pressed is bound to pause action.
-     *
      * @param inputCode of the key pressed
      * @return boolean based on the action being requested or not
      */
-    boolean mapToPause(int inputCode);
+    fun mapToPause(inputCode: Int): Boolean
 
     /**
      * Checks if the inputCode of the key pressed is bound to resume action.
-     *
      * @param inputCode of the key pressed
      * @return boolean based on the action being requested or not
      */
-    boolean mapToUnPause(int inputCode);
+    fun mapToUnPause(inputCode: Int): Boolean
 }
