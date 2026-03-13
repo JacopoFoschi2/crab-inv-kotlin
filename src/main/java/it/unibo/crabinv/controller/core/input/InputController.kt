c6d44bafa46ec4e5ddd3782a533e9bcff3a2e1e4
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-13 10:33:08 +0100

    refactor: converted InputController.java to Kotlin

--- src/main/java/it/unibo/crabinv/controller/core/input/InputController.kt
+++ src/main/java/it/unibo/crabinv/controller/core/input/InputController.kt
@@ -1,31 +1,27 @@
-package it.unibo.crabinv.controller.core.input;
+package it.unibo.crabinv.controller.core.input
 
-import it.unibo.crabinv.model.core.input.InputSnapshot;
+import it.unibo.crabinv.model.core.input.InputSnapshot
 
 /**
  * Contract for a controller for the User input.
- * Must use an {@link InputMapper} to bind keys to actions.
+ * Must use an [InputMapper] to bind keys to actions.
  */
-public interface InputController {
-
+interface InputController {
     /**
      * Manages the keypress.
-     *
      * @param keyCode of the key pressed
      */
-    void onKeyPressed(int keyCode);
+    fun onKeyPressed(keyCode: Int)
 
     /**
      * Manages the key release.
-     *
      * @param keyCode of the key released
      */
-    void onKeyReleased(int keyCode);
+    fun onKeyReleased(keyCode: Int)
 
     /**
-     * Returns the {@link InputSnapshot} to be used by the game logic.
-     *
+     * Returns the [InputSnapshot] to be used by the game logic.
      * @return the Input Snapshot
      */
-    InputSnapshot getInputState();
+    val inputState: InputSnapshot?
 }
