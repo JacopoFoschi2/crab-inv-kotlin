9829e4b8c19ecef28d7b8ce8b468be0362659d6e
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-13 09:49:22 +0100

    refactor: converted EntityController.java to kotlin

--- src/main/java/it/unibo/crabinv/controller/entities/entity/EntityController.kt
+++ src/main/java/it/unibo/crabinv/controller/entities/entity/EntityController.kt
@@ -1,39 +1,37 @@
-package it.unibo.crabinv.controller.entities.entity;
+package it.unibo.crabinv.controller.entities.entity
 
 /**
  * Provides methods to control an entity standardizing the way they should be controlled.
  */
-public interface EntityController {
-
+interface EntityController {
     /**
      * @return if the entity is alive
      */
-    boolean isAlive();
+    val isAlive: Boolean
 
     /**
      * Make the entity suffer the inputted amount of damage.
-     *
      * @param damage the damage the entity should suffer
      */
-    void takeDamage(int damage);
+    fun takeDamage(damage: Int)
 
     /**
      * @return the current health of the entity
      */
-    int getHealth();
+    val health: Int
 
     /**
      * @return the max health of the entity
      */
-    int getMaxHealth();
+    val maxHealth: Int
 
     /**
      * @return the x coordinate of the entity
      */
-    double getX();
+    val x: Double
 
     /**
      * @return the y coordinate of the entity
      */
-    double getY();
+    val y: Double
 }
