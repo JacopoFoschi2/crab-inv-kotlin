f4fd010565bb50fdbac5b93216d4fffcc7476a58
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-13 09:41:12 +0100

    refactor: converted LevelImpl.java to kotlin

--- src/main/java/it/unibo/crabinv/model/levels/LevelImpl.kt
+++ src/main/java/it/unibo/crabinv/model/levels/LevelImpl.kt
@@ -1,49 +1,28 @@
-package it.unibo.crabinv.model.levels;
+package it.unibo.crabinv.model.levels
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import it.unibo.crabinv.model.entities.enemies.wave.Wave;
-import it.unibo.crabinv.model.entities.enemies.wave.WaveProvider;
+import it.unibo.crabinv.model.entities.enemies.wave.Wave
+import it.unibo.crabinv.model.entities.enemies.wave.WaveProvider
+
 // Adapted from MoseBarbieri's LevelLogic
 
 /**
- * Implementation of {@link Level}.
+ * Implementation of [Level].
+ * @param waveProvider the provider of the wave
  */
-public final class LevelImpl implements Level {
-    private final WaveProvider waveProvider;
-    private Wave currentWave;
+class LevelImpl(
+    private val waveProvider: WaveProvider,
+) : Level {
+    override var currentWave: Wave? = null
+        private set
 
-    /**
-     * The levelImpl constructor.
-     *
-     * @param waveProvider the provider of the wave
-     */
-    public LevelImpl(final WaveProvider waveProvider) {
-        this.waveProvider = waveProvider;
-        advanceWave();
+    init {
+        advanceWave()
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @SuppressFBWarnings("EI_EXPOSE")//exposes internal representation by design
-    @Override
-    public Wave getCurrentWave() {
-        return this.currentWave;
+    override fun advanceWave() {
+        this.currentWave = if (waveProvider.hasMoreWaves()) waveProvider.getNextWave() else null
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void advanceWave() {
-        this.currentWave = waveProvider.hasMoreWaves() ? waveProvider.getNextWave() : null;
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean isLevelFinished() {
-        return currentWave == null;
-    }
+    override val isLevelFinished: Boolean
+        get() = currentWave == null
 }
