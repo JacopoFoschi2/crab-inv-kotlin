9c5cb7d4bed750da2fd11cbef38aae603d5b477c
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-13 09:40:38 +0100

    refactor: converted LevelFactory.java to kotlin

--- src/main/java/it/unibo/crabinv/model/levels/LevelFactory.kt
+++ src/main/java/it/unibo/crabinv/model/levels/LevelFactory.kt
@@ -1,20 +1,22 @@
-package it.unibo.crabinv.model.levels;
+package it.unibo.crabinv.model.levels
 
-import it.unibo.crabinv.model.entities.enemies.EnemyFactory;
-import it.unibo.crabinv.model.entities.enemies.rewardservice.RewardsService;
+import it.unibo.crabinv.model.entities.enemies.EnemyFactory
+import it.unibo.crabinv.model.entities.enemies.rewardservice.RewardsService
 
 /**
  * It's the interface that makes the factory method.
  */
-@FunctionalInterface
-public interface LevelFactory {
+fun interface LevelFactory {
     /**
      * It's the method which creates the level.
-     *
      * @param levelId the level id
      * @param enemyFactory the enemy factory that populates it
      * @param rewardsService the service that gives the player the currency
      * @return Level: the instance of the level that you want
      */
-    Level createLevel(int levelId, EnemyFactory enemyFactory, RewardsService rewardsService);
+    fun createLevel(
+        levelId: Int,
+        enemyFactory: EnemyFactory?,
+        rewardsService: RewardsService?,
+    ): Level?
 }
