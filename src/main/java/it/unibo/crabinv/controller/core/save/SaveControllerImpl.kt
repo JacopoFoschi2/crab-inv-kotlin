1f69da2f7c70dbe786b21059aecf8538478eb3bc
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-13 15:20:11 +0100

    refactor: converted SaveControllerImpl.java to Kotlin

--- src/main/java/it/unibo/crabinv/controller/core/save/SaveControllerImpl.kt
+++ src/main/java/it/unibo/crabinv/controller/core/save/SaveControllerImpl.kt
@@ -1,87 +1,57 @@
-package it.unibo.crabinv.controller.core.save;
+package it.unibo.crabinv.controller.core.save
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import it.unibo.crabinv.model.core.save.Save;
-import it.unibo.crabinv.core.persistence.repository.SaveRepository;
-
-import java.io.IOException;
-import java.util.List;
-import java.util.Objects;
-import java.util.UUID;
+import it.unibo.crabinv.core.persistence.repository.SaveRepository
+import it.unibo.crabinv.model.core.save.Save
+import java.io.IOException
+import java.util.Objects
+import java.util.UUID
 
 /**
- * Implementation of {@link SaveController}.
- *
- * @param saveRepository the {@link SaveRepository} used by the {@link SaveControllerImpl}
+ * Implementation of [SaveController].
+ * @param saveRepository the [SaveRepository] used by the [SaveControllerImpl]
  */
-@SuppressFBWarnings("EI_EXPOSE")//exposes internal representation by design
-public record SaveControllerImpl(SaveRepository saveRepository) implements SaveController {
-
-    /**
-     * Constructor for {@link SaveControllerImpl}.
-     *
-     * @param saveRepository the {@link SaveRepository} to be used
-     */
-    @SuppressFBWarnings("EI_EXPOSE_REP2") //dependencies are injected and owned by callergit
-    public SaveControllerImpl(final SaveRepository saveRepository) {
-        this.saveRepository = Objects.requireNonNull(saveRepository);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Save saveControlAndLoad() throws IOException {
-        final List<Save> saveList = this.saveRepository.list();
-        if (saveList.isEmpty()) {
-            return createSave();
-        } else if (saveList.size() == 1) {
-            return saveList.getFirst();
+class SaveControllerImpl(
+    saveRepository: SaveRepository?,
+) : SaveController {
+    @Throws(IOException::class)
+    override fun saveControlAndLoad(): Save {
+        val saveList = this.saveRepository.list()
+        return if (saveList.isEmpty()) {
+            createSave()
+        } else if (saveList.size == 1) {
+            saveList.first()
         } else {
-            return selectSave(saveList);
+            selectSave(saveList)
         }
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Save createSave() throws IOException {
-        final Save newSaveFile = this.saveRepository.newSave();
-        this.saveRepository.saveSaveFile(newSaveFile);
-        return newSaveFile;
+    @Throws(IOException::class)
+    override fun createSave(): Save {
+        val newSaveFile = this.saveRepository.newSave()
+        this.saveRepository.saveSaveFile(newSaveFile)
+        return newSaveFile
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public Save selectSave(final List<Save> saveList) {
-        return saveList.getLast();
+    override fun selectSave(saveList: MutableList<Save>): Save {
+        return saveList.last()
         // Placeholder for user-directed save selection if implemented
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void updateSave(final Save save) throws IOException {
-        this.saveRepository.saveSaveFile(save);
+    @Throws(IOException::class)
+    override fun updateSave(save: Save) {
+        this.saveRepository.saveSaveFile(save)
     }
 
     /**
      * {@inheritDoc}
      */
-    @Override
-    public Save loadSave(final UUID saveId) throws IOException {
-        return this.saveRepository.loadSaveFile(saveId);
+    @Throws(IOException::class)
+    override fun loadSave(saveId: UUID): Save = this.saveRepository.loadSaveFile(saveId)
+
+    @Throws(IOException::class)
+    override fun deleteSave(saveId: UUID) {
+        this.saveRepository.delete(saveId)
     }
 
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public void deleteSave(final UUID saveId) throws IOException {
-        this.saveRepository.delete(saveId);
-    }
+    val saveRepository: SaveRepository = Objects.requireNonNull<SaveRepository>(saveRepository)
 }
