36e6ea98b2b430022bdb59cce750c39b07e544d6
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-18 10:31:09 +0100

    refactor: converted WaveBeta.java to Kotlin

--- src/main/java/it/unibo/crabinv/model/entities/enemies/wavetypes/WaveBeta.kt
+++ src/main/java/it/unibo/crabinv/model/entities/enemies/wavetypes/WaveBeta.kt
@@ -1,53 +1,48 @@
-package it.unibo.crabinv.model.entities.enemies.wavetypes;
+package it.unibo.crabinv.model.entities.enemies.wavetypes
 
-import it.unibo.crabinv.model.entities.enemies.EnemyFactory;
-import it.unibo.crabinv.model.entities.enemies.EnemyType;
-import it.unibo.crabinv.model.entities.enemies.rewardservice.RewardsService;
-import it.unibo.crabinv.model.entities.enemies.wave.WaveImpl;
-
-import java.util.List;
+import it.unibo.crabinv.model.entities.enemies.EnemyFactory
+import it.unibo.crabinv.model.entities.enemies.EnemyType
+import it.unibo.crabinv.model.entities.enemies.rewardservice.RewardsService
+import it.unibo.crabinv.model.entities.enemies.wave.WaveImpl
 
 /**
- * Preset of a {@link WaveImpl}.
+ * Preset of a [WaveImpl].
+ * @param enemyFactory   the [EnemyFactory] used by the [WaveBeta]
+ * @param rewardsService [RewardsService] used by the [WaveBeta]
+ * @param spawnYNorm     the Y-axis coordinates spawn
+ * @param bottomXNorm    the Y-axis coordinates of the bottom border
+ * @param maxSpawnSlots  the max number of spawn slots
  */
-public final class WaveBeta extends WaveImpl {
-
-    /**
-     * Constructor of the {@link WaveBeta}.
-     *
-     * @param enemyFactory   the {@link EnemyFactory} used by the {@link WaveBeta}
-     * @param rewardsService {@link RewardsService} used by the {@link WaveBeta}
-     * @param spawnYNorm     the Y-axis coordinates spawn
-     * @param bottomXNorm    the Y-axis coordinates of the bottom border
-     * @param maxSpawnSlots  the max number of spawn slots
-     */
-    public WaveBeta(final EnemyFactory enemyFactory,
-                    final RewardsService rewardsService,
-                    final double spawnYNorm,
-                    final double bottomXNorm,
-                    final int maxSpawnSlots) {
-        super(
-                List.of(EnemyType.SERVANT,
-                        EnemyType.SERVANT,
-                        EnemyType.SERVANT,
-                        EnemyType.SERVANT,
-                        EnemyType.SERVANT,
-                        EnemyType.SERVANT,
-                        EnemyType.SERVANT,
-                        EnemyType.SERVANT),
-                List.of(WaveSlot.S2.getWaveSlot(),
-                        WaveSlot.S3.getWaveSlot(),
-                        WaveSlot.S4.getWaveSlot(),
-                        WaveSlot.S5.getWaveSlot(),
-                        WaveSlot.S6.getWaveSlot(),
-                        WaveSlot.S7.getWaveSlot(),
-                        WaveSlot.S8.getWaveSlot(),
-                        WaveSlot.S9.getWaveSlot()),
-                enemyFactory,
-                rewardsService,
-                maxSpawnSlots,
-                spawnYNorm,
-                bottomXNorm
-        );
-    }
-}
+class WaveBeta(
+    enemyFactory: EnemyFactory,
+    rewardsService: RewardsService,
+    spawnYNorm: Double,
+    bottomXNorm: Double,
+    maxSpawnSlots: Int,
+) : WaveImpl(
+        listOf<EnemyType?>(
+            EnemyType.SERVANT,
+            EnemyType.SERVANT,
+            EnemyType.SERVANT,
+            EnemyType.SERVANT,
+            EnemyType.SERVANT,
+            EnemyType.SERVANT,
+            EnemyType.SERVANT,
+            EnemyType.SERVANT,
+        ),
+        listOf<Int?>(
+            WaveSlot.S2.waveSlot,
+            WaveSlot.S3.waveSlot,
+            WaveSlot.S4.waveSlot,
+            WaveSlot.S5.waveSlot,
+            WaveSlot.S6.waveSlot,
+            WaveSlot.S7.waveSlot,
+            WaveSlot.S8.waveSlot,
+            WaveSlot.S9.waveSlot,
+        ),
+        enemyFactory,
+        rewardsService,
+        maxSpawnSlots,
+        spawnYNorm,
+        bottomXNorm,
+    )
