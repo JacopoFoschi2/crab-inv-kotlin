6c7d56e8d59f8dd97406d43237943ee1c14c48dd
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-18 10:17:39 +0100

    refactor: converted EnemyRewardService.java to Kotlin

--- src/main/java/it/unibo/crabinv/model/entities/enemies/rewardservice/EnemyRewardService.kt
+++ src/main/java/it/unibo/crabinv/model/entities/enemies/rewardservice/EnemyRewardService.kt
@@ -1,32 +1,16 @@
-package it.unibo.crabinv.model.entities.enemies.rewardservice;
+package it.unibo.crabinv.model.entities.enemies.rewardservice
 
-import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
-import it.unibo.crabinv.model.entities.enemies.Enemy;
-import it.unibo.crabinv.model.core.save.GameSession;
+import it.unibo.crabinv.model.core.save.GameSession
+import it.unibo.crabinv.model.entities.enemies.Enemy
 
 /**
  * It's the implementation of the reward service.
+ * @param profile it's the profile needed to give the reward
  */
-public final class EnemyRewardService implements RewardsService {
-    private final GameSession profile;
-
-    /**
-     * It's the constructor of the reward service.
-     *
-     * @param profile it's the profile needed to give the reward
-     */
-    @SuppressFBWarnings("EI_EXPOSE_REP2") //dependencies are injected and owned by caller
-    public EnemyRewardService(final GameSession profile) {
-        this.profile = profile;
-    }
-
-    /**
-     * {@inheritDoc}
-     *
-     * @param enemy the dead enemy
-     */
-    @Override
-    public void rewardEnemyDeath(final Enemy enemy) {
-        profile.addCurrency(enemy.getReward());
+class EnemyRewardService(
+    private val profile: GameSession,
+) : RewardsService {
+    override fun rewardEnemyDeath(enemy: Enemy) {
+        profile.addCurrency(enemy.getReward())
     }
 }
