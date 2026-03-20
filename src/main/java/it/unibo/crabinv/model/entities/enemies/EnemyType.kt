53441a76e8b0b7d0fa1542c05ad676d38b3ead61
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-20 13:46:00 +0100

    refactor: converted EnemyType.java to Kotlin

--- src/main/java/it/unibo/crabinv/model/entities/enemies/EnemyType.kt
+++ src/main/java/it/unibo/crabinv/model/entities/enemies/EnemyType.kt
@@ -1,29 +1,14 @@
-package it.unibo.crabinv.model.entities.enemies;
+package it.unibo.crabinv.model.entities.enemies
 
 /**
  * It's the identifier of the enemy.
+ * @param rewardForKill the reward for the death of the enemy
  */
-public enum EnemyType {
-    SERVANT(10);
-
-    private final int rewardForKill;
-
+enum class EnemyType(
     /**
-     * It's the constructor of the enemyType.
-     *
-     * @param rewardForKill the reward for the death of the enemy
-     */
-    EnemyType(final int rewardForKill) {
-        this.rewardForKill = rewardForKill;
-    }
-
-    /**
-     * Getter for the reward.
-     *
      * @return the actual value for the reward
      */
-    public int getRewardForKill() {
-        return rewardForKill;
-    }
-
+    val rewardForKill: Int,
+) {
+    SERVANT(10),
 }
