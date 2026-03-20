cf0b9ae23bff6521ebcc68958045e97559ac4c75
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-20 10:17:19 +0100

    refactor: converted EnemyBulletFactory.java to Kotlin

--- src/main/java/it/unibo/crabinv/model/entities/bullets/EnemyBulletFactory.kt
+++ src/main/java/it/unibo/crabinv/model/entities/bullets/EnemyBulletFactory.kt
@@ -1,36 +1,28 @@
-package it.unibo.crabinv.model.entities.bullets;
+package it.unibo.crabinv.model.entities.bullets
 
 /**
  * It's the enemyBulletFactory that handles it.
  */
-public final class EnemyBulletFactory implements BulletFactory {
-    private static final double ENEMY_BULLET_RADIUS = 0.01;
-    private static final double ENEMY_BULLET_SPEED = 0.01;
-    private static final int ENEMY_BULLET_MAX_HEALTH = 1;
+class EnemyBulletFactory : BulletFactory {
+    override fun createBullet(
+        x: Double,
+        y: Double,
+        minBound: Double,
+        maxBound: Double,
+    ): Bullet =
+        BulletEnemy(
+            ENEMY_BULLET_MAX_HEALTH,
+            x,
+            y,
+            ENEMY_BULLET_RADIUS,
+            ENEMY_BULLET_SPEED,
+            minBound,
+            maxBound,
+        )
 
-    /**
-     * {@inheritDoc}
-     *
-     * @param x the horizontal position of the bullet
-     * @param y the vertical position of the bullet
-     * @param minBound the min bound that must not be touched
-     * @param maxBound the max bound that must not be touched
-     *
-     * @return BulLet, the bullet created
-     */
-    @Override
-    public Bullet createBullet(
-            final double x,
-            final double y,
-            final double minBound,
-            final double maxBound) {
-        return new BulletEnemy(
-                ENEMY_BULLET_MAX_HEALTH,
-                x,
-                y,
-                ENEMY_BULLET_RADIUS,
-                ENEMY_BULLET_SPEED,
-                minBound,
-                maxBound);
+    companion object {
+        private const val ENEMY_BULLET_RADIUS = 0.01
+        private const val ENEMY_BULLET_SPEED = 0.01
+        private const val ENEMY_BULLET_MAX_HEALTH = 1
     }
 }
