f848a3981d569ca4a2f6a6b94bc2a07ec453b1fa
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-20 10:17:33 +0100

    refactor: converted PlayerBulletFactory.java to Kotlin

--- src/main/java/it/unibo/crabinv/model/entities/bullets/PlayerBulletFactory.kt
+++ src/main/java/it/unibo/crabinv/model/entities/bullets/PlayerBulletFactory.kt
@@ -1,36 +1,28 @@
-package it.unibo.crabinv.model.entities.bullets;
+package it.unibo.crabinv.model.entities.bullets
 
 /**
  * It's the playerBulletFactory that creates the bullets of the player.
  */
-public final class PlayerBulletFactory implements BulletFactory {
-    private static final double PLAYER_BULLET_RADIUS = 0.01;
-    private static final double PLAYER_BULLET_SPEED = 0.01;
-    private static final int PLAYER_BULLET_MAX_HEALTH = 1;
+class PlayerBulletFactory : BulletFactory {
+    override fun createBullet(
+        x: Double,
+        y: Double,
+        minBound: Double,
+        maxBound: Double,
+    ): Bullet =
+        BulletPlayer(
+            PLAYER_BULLET_MAX_HEALTH,
+            x,
+            y,
+            PLAYER_BULLET_RADIUS,
+            PLAYER_BULLET_SPEED,
+            minBound,
+            maxBound,
+        )
 
-    /**
-     * {@inheritDoc}
-     *
-     * @param x the horizontal position of the bullet
-     * @param y the vertical position of the bullet
-     * @param minBound the min bound that must not be touched
-     * @param maxBound the max bound that must not be touched
-     *
-     * @return Bullet of the player
-     */
-    @Override
-    public Bullet createBullet(
-            final double x,
-            final double y,
-            final double minBound,
-            final double maxBound) {
-        return new BulletPlayer(
-                PLAYER_BULLET_MAX_HEALTH,
-                x,
-                y,
-                PLAYER_BULLET_RADIUS,
-                PLAYER_BULLET_SPEED,
-                minBound,
-                maxBound);
+    companion object {
+        private const val PLAYER_BULLET_RADIUS = 0.01
+        private const val PLAYER_BULLET_SPEED = 0.01
+        private const val PLAYER_BULLET_MAX_HEALTH = 1
     }
 }
