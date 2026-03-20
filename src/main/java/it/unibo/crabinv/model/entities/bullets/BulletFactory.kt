0be2d91f90fb27d43fa5b445555ece8676536235
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-20 10:17:06 +0100

    refactor: converted BulletFactory.java to Kotlin

--- src/main/java/it/unibo/crabinv/model/entities/bullets/BulletFactory.kt
+++ src/main/java/it/unibo/crabinv/model/entities/bullets/BulletFactory.kt
@@ -1,18 +1,21 @@
-package it.unibo.crabinv.model.entities.bullets;
+package it.unibo.crabinv.model.entities.bullets
 
 /**
  * Interface that establishes the factory of the bullet.
  */
-@FunctionalInterface
-public interface BulletFactory {
+fun interface BulletFactory {
     /**
      * The method that creates the bullet.
-     *
      * @param x the horizontal position of the bullet
      * @param y the vertical position of the bullet
      * @param minBound the min bound that must not be touched
      * @param maxBound the max bound that must not be touched
      * @return Bullet, the actual bullet
      */
-    Bullet createBullet(double x, double y, double minBound, double maxBound);
+    fun createBullet(
+        x: Double,
+        y: Double,
+        minBound: Double,
+        maxBound: Double,
+    ): Bullet
 }
