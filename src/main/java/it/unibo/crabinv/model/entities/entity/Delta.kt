1f86aa3b5c4f94a7eb901c2c642d6edbf8ee8f75
Jacopo Foschi <jacopo.foschi2@studio.unibo.it>
2026-03-20 13:30:49 +0100

    refactor: converted Delta.java to Kotlin

--- src/main/java/it/unibo/crabinv/model/entities/entity/Delta.kt
+++ src/main/java/it/unibo/crabinv/model/entities/entity/Delta.kt
@@ -1,24 +1,16 @@
-package it.unibo.crabinv.model.entities.entity;
+package it.unibo.crabinv.model.entities.entity
 
 /**
  * Provides all the deltas that the entity that wishes to move should handle.
  * The selected delta affects the movement by either decreasing or increasing its coordinate in the axis.
  */
-public enum Delta {
-    DECREASE(-1),
-    NO_ACTION(0),
-    INCREASE(1);
-
-    private final int value;
-
-    Delta(final int value) {
-        this.value = value;
-    }
-
+enum class Delta(
     /**
      * @return the value of delta
      */
-    public int getValue() {
-        return value;
-    }
+    val value: Int,
+) {
+    DECREASE(-1),
+    NO_ACTION(0),
+    INCREASE(1),
 }
